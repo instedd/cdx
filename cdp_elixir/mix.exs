35c365ad0e5ed5d82ab7ec208ea8e9fa4094eb56
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-03-25 17:17:54 -0300

    Added Elixir backend.

---
+++ cdp_elixir/mix.exs
@@ -0,0 +1,23 @@
+defmodule CdpElixir.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :cdp_elixir,
+      version: "0.0.1",
+      build_per_environment: true,
+      dynamos: [CdpElixir.Dynamo],
+      compilers: [:elixir, :dynamo, :app],
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [ applications: [:cowboy, :dynamo],
+      mod: { CdpElixir, [] } ]
+  end
+
+  defp deps do
+    [ { :cowboy, github: "extend/cowboy" },
+      { :dynamo, "~> 0.1.0-dev", github: "elixir-lang/dynamo" } ]
+  end
+end
