20e2dab928f3d94ed0bd8535ac3f87d4cc23d061
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-04-16 16:28:59 -0300

    Added settings.yml and guisso.yml to capistrano linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/settings.yml config/guisso.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
