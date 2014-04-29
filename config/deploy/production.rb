1545ff591b7f956488b5123821e4c5c889cdb697
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-04-29 11:00:41 -0300

    Capistrano: updated staging and production servers urls

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'staging.instedd.org', user: 'ubuntu', roles: %w{web app} #, my_property: :my_value
+server 'cdp-stg.instedd.org', user: 'ubuntu', roles: %w{web app} #, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
