991608b08a931a0649125d76068372839ec6ded8
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-05-16 09:43:47 -0300

    Manifests takes its version number from the definition.

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -2,13 +2,13 @@ class Manifest < ActiveRecord::Base
   has_and_belongs_to_many :device_models
 
   before_save :update_models
-  # before_save :update_version
+  before_save :update_version
 
   def update_models
-    JSON.parse(self.definition)["device_models"].each { |model| self.device_models << (DeviceModel.find_or_create_by(name: model))}
+    self.device_models = JSON.parse(self.definition)["device_models"].map { |model| DeviceModel.find_or_create_by(name: model)}
   end
 
-  # def update_version
-  #   self.version = 1
-  # end
+  def update_version
+    self.version = JSON.parse(self.definition)["version"]
+  end
 end
--- spec/controllers/manifests_controller_spec.rb
+++ spec/controllers/manifests_controller_spec.rb
@@ -5,12 +5,24 @@ describe ManifestsController do
   before(:each) { sign_in current_user }
 
   it "creates a device model named according to the manifest" do
-    manifest = '{"device_models" : ["foo"] }'
+    manifest = '{"device_models" : ["foo"]}'
     post :create, manifest: {definition: manifest}
 
     Manifest.count.should eq(1)
     Manifest.first.definition.should eq(manifest)
     DeviceModel.count.should eq(1)
     Manifest.first.device_models.first.name.should eq("foo")
+  end
+
+  it "updates it's version number" do
+    post :create, manifest: {definition: '{"device_models" : ["foo"], "version" : 1}'}
+
+    Manifest.first.version.should eq(1)
+
+    put :update, id: Manifest.first.id, manifest: {definition: '{"device_models" : ["foo"], "version" : 2}'}
+
+    Manifest.count.should eq(1)
+    DeviceModel.count.should eq(1)
+    Manifest.first.version.should eq(2)
   end
 end
