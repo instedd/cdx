5bd7024b305e2010a1bc534d2514b0e076bee892
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-05-16 10:16:17 -0300

    Manifests leaves no orphan DeviceModels.

--- app/models/manifest.rb
+++ app/models/manifest.rb
@@ -3,6 +3,7 @@ class Manifest < ActiveRecord::Base
 
   before_save :update_models
   before_save :update_version
+  after_destroy :ensure_no_orphan_models
 
   def update_models
     self.device_models = JSON.parse(self.definition)["device_models"].map { |model| DeviceModel.find_or_create_by(name: model)}
@@ -10,5 +11,9 @@ class Manifest < ActiveRecord::Base
 
   def update_version
     self.version = JSON.parse(self.definition)["version"]
+  end
+
+  def ensure_no_orphan_models
+    DeviceModel.includes(:manifests).where('device_models_manifests.manifest_id' => nil).destroy_all
   end
 end
