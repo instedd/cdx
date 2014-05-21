65d735ebb4d4c3be78fe1bbeae43814078735b00
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-05-21 11:00:27 -0300

    Removed two migratinos that left the DB in the same way

--- db/migrate/20140520153146_rename_device_model_to_model.rb
+++
@@ -1,8 +0,0 @@
-class RenameDeviceModelToModel < ActiveRecord::Migration
-  def change
-    rename_table :device_models, :models
-    rename_column(:device_models_manifests, :device_model_id, :model_id)
-    rename_column(:devices, :device_model_id, :model_id)
-    rename_table :device_models_manifests, :manifests_models
-  end
-end
--- db/migrate/20140520163711_rename_model_to_device_model.rb
+++
@@ -1,8 +0,0 @@
-class RenameModelToDeviceModel < ActiveRecord::Migration
-  def change
-    rename_table :models, :device_models
-    rename_column :manifests_models, :model_id, :device_model_id
-    rename_column :devices, :model_id, :device_model_id
-    rename_table :manifests_models, :device_models_manifests
-  end
-end
