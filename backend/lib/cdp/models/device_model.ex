1a06a4aba2b2e01ffa9c743439b8ec9339b39e07
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-05-21 10:55:40 -0300

    Fix for Ecto automatic key generation error.

--- backend/lib/cdp/models/device_model.ex
+++ backend/lib/cdp/models/device_model.ex
@@ -2,7 +2,7 @@ defmodule DeviceModel do
   use Ecto.Model
 
   queryable "device_models" do
-    has_many(:device_models_manifests, DeviceModelsManifests)
+    has_many(:device_models_manifests, DeviceModelsManifests, foreign_key: :device_model_id)
     has_many(:devices, Device)
     field :name
   end
