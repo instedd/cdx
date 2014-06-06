4e220d4e1379146667fecd17609e1d9900a3edf2
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-06-06 17:27:48 -0300

    Using postgresql with chicagoboss. User is hardcoded for now. Reading and deleting database events.

---
+++ cdxapi/src/controller/cdxapi_event_controller.erl
@@ -0,0 +1,31 @@
+-module(cdxapi_event_controller, [Req]).
+-compile(export_all).
+
+hello('GET', []) ->
+ {ok, [{events, "Hello, world!"}]}.
+
+foo('GET', []) ->
+  {output, "Hello, Foo!"}.
+
+bar('GET', []) ->
+  {json, [{events, "Hello, world!"}]}.
+
+list('GET', []) ->
+  Events = boss_db:find(event, []),
+  {ok, [{events, Events}]}.
+
+create('GET', []) ->
+  ok;
+create('POST', []) ->
+  EventId = Req:post_param("id"),
+  NewEvent = events:new(id, EventId),
+  case NewEvent:save() of
+    {ok, SavedEvent} ->
+      {redirect, [{action, "list"}]};
+    {error, ErrorList} ->
+      {ok, [{errors, ErrorList}, {new_msg, NewEvent}]}
+  end.
+
+goodbye('POST', []) ->
+  boss_db:delete(Req:post_param("event_id")),
+  {redirect, [{action, "list"}]}.
---
+++ cdxapi/src/model/events.erl
@@ -0,0 +1,2 @@
+-module(event, [Id, DeviceId, CreatedAt, UpdatedAt, Uuid, EventId, CustomFields, RawData, SensitiveData]).
+-compile(export_all).
