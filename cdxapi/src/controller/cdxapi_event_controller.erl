7b90968163f5c4be5f5aa769db34759c0f18a6bf
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-06-17 16:47:41 -0300

    Added support for ISO dates in erlang. Added mock test for event reporting.

--- cdxapi/src/controller/cdxapi_event_controller.erl
+++ cdxapi/src/controller/cdxapi_event_controller.erl
@@ -17,15 +17,40 @@ list('GET', []) ->
 create('GET', []) ->
   ok;
 create('POST', []) ->
-  EventId = Req:post_param("id"),
-  NewEvent = event:new(id, EventId),
-  case NewEvent:save() of
-    {ok, SavedEvent} ->
-      {redirect, [{action, "list"}]};
-    {error, ErrorList} ->
-      {ok, [{errors, ErrorList}, {new_msg, NewEvent}]}
-  end.
+% jsx:decode(<<"{\"library\": \"jsx\", \"awesome\": true}">>).
+% jsx:encode([{<<"library">>,<<"jsx">>},{<<"awesome">>,true}]).
+  io:format("req: ~p~n", [Req]),
+  {json, [{events, "Hello world!"}]}.
+  % EventId = Req:post_param("id"),
+  % NewEvent = event:new(id, EventId),
+  % case NewEvent:save() of
+  %   {ok, SavedEvent} ->
+  %     {redirect, [{action, "list"}]};
+  %   {error, ErrorList} ->
+  %     {ok, [{errors, ErrorList}, {new_msg, NewEvent}]}
+  % end.
 
 goodbye('POST', []) ->
   boss_db:delete(Req:post_param("event_id")),
   {redirect, [{action, "list"}]}.
+
+
+% #### to convert a utf8 binary containing a json string into an erlang term ####
+
+% ```erlang
+% 1> jsx:decode(<<"{\"library\": \"jsx\", \"awesome\": true}">>).
+% [{<<"library">>,<<"jsx">>},{<<"awesome">>,true}]
+% 2> jsx:decode(<<"[\"a\",\"list\",\"of\",\"words\"]">>).
+% [<<"a">>, <<"list">>, <<"of">>, <<"words">>]
+% ```
+
+% #### to convert an erlang term into a utf8 binary containing a json string ####
+
+% ```erlang
+% 1> jsx:encode([{<<"library">>,<<"jsx">>},{<<"awesome">>,true}]).
+% <<"{\"library\": \"jsx\", \"awesome\": true}">>
+% 2> jsx:encode(#{<<"library">> => <<"jsx">>, <<"awesome">> => true}).
+% <<"{\"awesome\":true,\"library\":\"jsx\"}">>
+% 3> jsx:encode([<<"a">>, <<"list">>, <<"of">>, <<"words">>]).
+% <<"[\"a\",\"list\",\"of\",\"words\"]">>
+% ```
