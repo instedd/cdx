8fd87f87a5ddfefa0e874988f8846b7ceee5d7e8
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-07-21 16:03:02 -0300

    Typo fixed.

--- app/models/concerns/event_field_definition.rb
+++ app/models/concerns/event_field_definition.rb
@@ -3,11 +3,11 @@ module EventFieldDefinition
 
   included do
     def self.sensitive_fields
-      @sensitive_fields ||= YAML.load_file('config/sensitive_fields.yml').with_indifferent_access[:sensitive_fields]
+      @sensitive_fields ||= YAML.load_file('config/event_fields.yml').with_indifferent_access[:sensitive_fields]
     end
 
     def self.searchable_fields
-      @searchable_fields ||= YAML.load_file('config/sensitive_fields.yml').with_indifferent_access[:searchable_fields].map do |definition|
+      @searchable_fields ||= YAML.load_file('config/event_fields.yml').with_indifferent_access[:searchable_fields].map do |definition|
         IndexedField.from definition
       end
     end
