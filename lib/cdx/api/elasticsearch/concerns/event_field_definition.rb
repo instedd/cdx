8ddae4be0cf9ad7a72740cde295c685bc773d374
Ary Borenszweig <aborenszweig@manas.com.ar>
2014-07-22 12:36:23 -0300

    Load event fields config file from Rails.root first, otherwise from gem

--- lib/cdx/api/elasticsearch/concerns/event_field_definition.rb
+++ lib/cdx/api/elasticsearch/concerns/event_field_definition.rb
@@ -3,13 +3,22 @@ module Cdx::Api::Elasticsearch::Concerns::EventFieldDefinition
 
   included do
     def self.sensitive_fields
-      @sensitive_fields ||= YAML.load_file('config/event_fields.yml').with_indifferent_access[:sensitive_fields]
+      @sensitive_fields ||= event_fields_config[:sensitive_fields]
     end
 
     def self.searchable_fields
-      @searchable_fields ||= YAML.load_file('config/event_fields.yml').with_indifferent_access[:searchable_fields].map do |definition|
+      @searchable_fields ||= event_fields_config[:searchable_fields].map do |definition|
         Cdx::Api::Elasticsearch::IndexedField.from definition
       end
+    end
+
+    def self.event_fields_config
+      config_filename = "#{Rails.root}/config/event_fields.yml"
+      unless File.exists?(config_filename)
+        config_filename = File.expand_path("../../../../../../config/event_fields.yml", __FILE__)
+      end
+
+      YAML.load_file(config_filename).with_indifferent_access
     end
   end
 end
