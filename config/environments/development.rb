1a7fb3793b6f91b7201d646068cd7d6d4b93a4ef
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-08-08 16:20:50 -0300

    Serve assets as a single file in development to fasten the response of the development environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Cdp::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.action_mailer.default_url_options = { host: Settings.host }
 end
