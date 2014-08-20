0a70697352c36149b885a38ba3a9c2e31cae2d73
Martin Verzilli <mverzilli@manas.com.ar>
2014-08-20 11:45:34 -0300

    Added started_at field, meaning when the test was done (as opposed to created_at, which is when the test enters the system

--- spec/models/cdx_api_spec.rb
+++ spec/models/cdx_api_spec.rb
@@ -54,8 +54,8 @@ describe Cdx::Api do
 
   describe "Filter" do
     it "should check for new events since a date" do
-      index results: [result: :positive], created_at: time(2013, 1, 1)
-      index results: [result: :negative], created_at: time(2013, 1, 2)
+      index results: [result: :positive], started_at: time(2013, 1, 1)
+      index results: [result: :negative], started_at: time(2013, 1, 2)
 
       response = query_events(since: time(2013, 1, 2))
 
@@ -73,8 +73,8 @@ describe Cdx::Api do
     it "should check for new events since a date in a differen time zone" do
       Time.zone = ActiveSupport::TimeZone["Asia/Seoul"]
 
-      index results: [result: :positive], created_at: 3.day.ago.at_noon.iso8601
-      index results: [result: :negative], created_at: 1.day.ago.at_noon.iso8601
+      index results: [result: :positive], started_at: 3.day.ago.at_noon.iso8601
+      index results: [result: :negative], started_at: 1.day.ago.at_noon.iso8601
 
       response = query_events(since: 2.day.ago.at_noon.iso8601)
 
@@ -90,8 +90,8 @@ describe Cdx::Api do
     end
 
     it "should check for new events util a date" do
-      index results: [result: :positive], created_at: time(2013, 1, 1)
-      index results: [result: :negative], created_at: time(2013, 1, 3)
+      index results: [result: :positive], started_at: time(2013, 1, 1)
+      index results: [result: :negative], started_at: time(2013, 1, 3)
 
       expect_one_result "positive", until: time(2013, 1, 2)
     end
