3b0a2ce562865d7705787d25b80ee9cd88a551d8
Martin Verzilli <mverzilli@manas.com.ar>
2014-09-03 11:50:50 -0300

    Moved device validation tests from the controller spec to the model spec.

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Device do
   it { should validate_presence_of :device_model }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :institution }
 end
