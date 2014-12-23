de74c8ce7c2e44ad260ce83f45a525898b51ca90
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2014-12-23 17:24:49 -0300

    Changing default authorized keys path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,6 @@ Cdp::Application.configure do
 
   config.action_mailer.default_url_options = { host: Settings.host }
 
-  config.authorized_keys_path = Rails.root.join('tmp/authorized_keys')
+  config.authorized_keys_path = Rails.root.join('tmp/ssh/authorized_keys')
   config.sync_dir_path = Rails.root.join('tmp/sync')
 end
