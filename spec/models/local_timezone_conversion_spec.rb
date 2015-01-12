52b8f7edae35acfdf96f63106ddf441016038e0b
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-12 14:06:09 -0300

    Testing conversion when there is not timezone

--- spec/models/local_timezone_conversion_spec.rb
+++ spec/models/local_timezone_conversion_spec.rb
@@ -3,16 +3,25 @@ require 'spec_helper'
 describe Cdx::Api::LocalTimeZoneConversion do
   include Cdx::Api::LocalTimeZoneConversion
 
-  describe 'keeps strings and integers unchanged' do
+  describe 'converting integers and strings' do
     it { expect(convert_timezone_if_date("1")).to eq "1" }
     it { expect(convert_timezone_if_date("100")).to eq "100" }
     it { expect(convert_timezone_if_date("foo")).to eq "foo" }
   end
 
-  describe 'assumes local timezone' do
-    it { expect(convert_timezone_if_date("1990-10-01")).to eq local(1990, 10, 1, 00, 00, 00) }
-    it { expect(convert_timezone_if_date("1990-10-01T02:30:24")).to eq local(1990, 10, 1, 02, 30, 24) }
-    it { expect(convert_timezone_if_date("1990-01-01T02:30:24Z-03:00")).to eq "1990-01-01T02:30:24Z-03:00" }
+  describe 'converting dates' do
+    context 'when there is a local timezone' do
+      before { Time.zone = ActiveSupport::TimeZone["Asia/Seoul"] }
+      it { expect(convert_timezone_if_date("1990-10-01")).to eq local(1990, 10, 1, 00, 00, 00) }
+      it { expect(convert_timezone_if_date("1990-10-01T02:30:24")).to eq local(1990, 10, 1, 02, 30, 24) }
+      it { expect(convert_timezone_if_date("1990-01-01T02:30:24Z-03:00")).to eq "1990-01-01T02:30:24Z-03:00" }
+    end
+    context 'when there is no local timezone' do
+      before { Time.zone = nil }
+      it { expect(convert_timezone_if_date("1990-10-01")).to eq "1990-10-01" }
+      it { expect(convert_timezone_if_date("1990-10-01T02:30:24")).to eq "1990-10-01T02:30:24" }
+      it { expect(convert_timezone_if_date("1990-01-01T02:30:24-03:00")).to eq "1990-01-01T02:30:24-03:00" }
+    end
   end
 
   def local(*args)
