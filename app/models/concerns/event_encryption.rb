e3df14753c09182c86dfca8ee4e3e271011d8464
Mariano Abel Coca <marianoabelcoca@gmail.com>
2015-01-20 10:44:21 -0300

    Added environment variables for event encryption keys.

--- app/models/concerns/event_encryption.rb
+++ app/models/concerns/event_encryption.rb
@@ -17,17 +17,17 @@ module EventEncryption
     private
 
     def secret_key
-      'a very secret key'
+      ENV['EVENT_SECRET_KEY'] || Devise.secret_key
     end
 
     def iv
       # OpenSSL::Cipher::Cipher.new('aes-256-cbc').random_iv
-      "\xD7\xCA\xD5\x9D\x1D\xC0I\x01Sf\xC8\xFBa\x88\xE1\x03"
+      ENV['EVENT_IV'] || "\xD7\xCA\xD5\x9D\x1D\xC0I\x01Sf\xC8\xFBa\x88\xE1\x03"
     end
 
     def salt
       # Time.now.to_i.to_s
-      "1403203711"
+      ENV['EVENT_SALT'] || "1403203711"
     end
   end
 end
