7aac68b3a8dedda488789698eb6df5183c06bc6c
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-21 17:43:53 -0300

    Using rsync
    Fixes instedd/cdx-sync-server#2

--- config/initializers/sync.rb
+++ config/initializers/sync.rb
@@ -14,13 +14,3 @@ CDXSync.instance_eval do
     '/usr/local/bin/rrsync'
   end
 end
-
-CDXSync::Client.class_eval do
-  def authorized_keys_entry(sync_dir)
-    #FIXME remove this.
-    #We need to use rrsync, but the problem is that sync_dir has paths local to this server,
-    #but not to the sshd-server. We need to add a translation for both paths, or use same path
-    #in both containers, or make the sync_dir aware of that mismatch, or have two sync_dirs - one local and one remote
-    "no-agent-forwarding,no-port-forwarding,no-pty,no-user-rc,no-X11-forwarding #{public_key}"
-  end
-end
