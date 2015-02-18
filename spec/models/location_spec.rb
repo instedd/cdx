a33d1eb05a1d0c9a64220ac99d5adced35f2498f
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-02-18 12:36:46 -0300

    Adding test for location parent update

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -25,4 +25,20 @@ describe Location do
     location2 = Location.create parent: location1, geo_id: '00AA'
     location2.errors.messages.should include(:geo_id=>["has already been taken"])
   end
+
+  it "updates parent consistently" do
+    location1 = Location.create! parent: root_location, geo_id: '00AA'
+    location2 = Location.create! parent: location1, geo_id: '00AB'
+    location3 = Location.create! parent: location2, geo_id: '00AC'
+
+    location2.parent.should eq location1
+    location2.admin_level.should eq 2
+    location3.admin_level.should eq 3
+
+    location2.update_attributes!(parent: root_location)
+
+    location2.parent.should eq root_location
+    location2.admin_level.should eq 1
+    location3.reload.admin_level.should eq 2
+  end
 end
