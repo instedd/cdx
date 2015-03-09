99c10a0a5ee51d46c77ae093dad046bf010e1568
Santiago Palladino <spalladino@gmail.com>
2015-03-09 15:58:20 -0300

    Added specs for CSV parser, excluding processing #144

--- spec/models/csv_event_parser_spec.rb
+++ spec/models/csv_event_parser_spec.rb
@@ -22,6 +22,47 @@ describe CSVEventParser do
     client.search(index: institution.elasticsearch_index_name)["hits"]["hits"]
   end
 
+  it "parses a single line CSV" do
+    data = CSVEventParser.new.load <<-CSV.strip_heredoc
+      error_code;result
+      4002;negative
+    CSV
+
+    data.should eq({
+      error_code: '4002',
+      result: 'negative'
+    }.stringify_keys)
+  end
+
+  it "parses a multi line CSV" do
+    data = CSVEventParser.new.load_all <<-CSV.strip_heredoc
+      error_code;result
+      4002;negative
+      8002;positive
+    CSV
+
+    data.should eq([
+      {error_code: '4002', result: 'negative'},
+      {error_code: '8002', result: 'positive'}
+    ].map(&:stringify_keys))
+  end
+
+  it "looks up a field given its path" do
+    CSVEventParser.new.lookup('result', {'error_code' => '4002', 'result' => 'negative'}).should eq('negative')
+  end
+
+  it "does not support collections in lookup" do
+    expect {
+      CSVEventParser.new.lookup('results[*].result', {'error_code' => '4002', 'result' => 'negative'})
+    }.to raise_error
+  end
+
+  it "does not support nested fields in lookup" do
+    expect {
+      CSVEventParser.new.lookup('error.error_code', {'error_code' => '4002', 'result' => 'negative'})
+    }.to raise_error
+  end
+
   pending 'parses a csv' do
     manifest = Manifest.create! definition: %{
       {
