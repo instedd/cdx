1776415dbc344ac56ea2514655d764a9d42b3bfb
Santiago Palladino <spalladino@gmail.com>
2015-03-10 14:03:40 -0300

    Import JSON files from sync dir as well #144

--- spec/models/device_event_importer_spec.rb
+++ spec/models/device_event_importer_spec.rb
@@ -12,47 +12,76 @@ describe DeviceEventImporter do
     sync_dir.ensure_client_sync_paths! device.secret_key
   end
 
-  before(:each) do
-    File.open(File.join(sync_dir.inbox_path(device.secret_key), "#{DateTime.now.strftime('%Y%m%d%H%M%S')}.csv"), "w") do |io|
-      io << %{error_code;result\n0;positive\n1;negative}
+  def write_file(content, extension)
+    File.open(File.join(sync_dir.inbox_path(device.secret_key), "#{DateTime.now.strftime('%Y%m%d%H%M%S')}.#{extension}"), "w") do |io|
+      io << content
     end
   end
 
-  it 'parses a csv from sync dir' do
-    manifest = Manifest.create! definition: %{
-      {
-        "metadata": {
-          "version": "1",
-          "api_version": "1.1.0",
-          "device_models": "#{device.device_model.name}",
-          "source" : { "type" : "csv"}
+  let(:manifest) do Manifest.create! definition: %{
+    {
+      "metadata": {
+        "version": "1",
+        "api_version": "1.1.0",
+        "device_models": "#{device.device_model.name}",
+        "source" : { "type" : "#{source}"}
+      },
+      "field_mapping" : {"event" : [{
+          "target_field" : "error_code",
+          "source" : {"lookup": "error_code"},
+          "core" : true,
+          "type" : "integer"
         },
-        "field_mapping" : {"event" : [{
-            "target_field" : "error_code",
-            "source" : {"lookup": "error_code"},
-            "core" : true,
-            "type" : "integer"
-          },
-          {
-            "target_field" : "result",
-            "source" : {"lookup": "result"},
-            "core" : true,
-            "type" : "enum",
-            "options" : [ "positive", "negative" ]
-          }
-        ]}
-      }
+        {
+          "target_field" : "result",
+          "source" : {"lookup": "result"},
+          "core" : true,
+          "type" : "enum",
+          "options" : [ "positive", "negative" ]
+        }
+      ]}
     }
+  }
+  end
 
-    DeviceEventImporter.new.import_from sync_dir
+  context "csv" do
 
-    events = all_elasticsearch_events_for(device.institution).sort_by { |event| event["_source"]["error_code"] }
-    event = events.first["_source"]
-    event["error_code"].should eq("0")
-    event["result"].should eq("positive")
-    event = events.last["_source"]
-    event["error_code"].should eq("1")
-    event["result"].should eq("negative")
+    let(:source) { "csv" }
+
+    it 'parses a csv from sync dir' do
+      manifest
+      write_file(%{error_code;result\n0;positive\n1;negative}, 'csv')
+      DeviceEventImporter.new.import_from sync_dir
+
+      events = all_elasticsearch_events_for(device.institution).sort_by { |event| event["_source"]["error_code"] }
+      event = events.first["_source"]
+      event["error_code"].should eq("0")
+      event["result"].should eq("positive")
+      event = events.last["_source"]
+      event["error_code"].should eq("1")
+      event["result"].should eq("negative")
+    end
+
+  end
+
+  context "json" do
+
+    let(:source) { "json" }
+
+    it 'parses a json from sync dir' do
+      manifest
+      write_file('[{"error_code": "0", "result": "positive"}, {"error_code": "1", "result": "negative"}]', 'json')
+      DeviceEventImporter.new("*.json").import_from sync_dir
+
+      events = all_elasticsearch_events_for(device.institution).sort_by { |event| event["_source"]["error_code"] }
+      event = events.first["_source"]
+      event["error_code"].should eq("0")
+      event["result"].should eq("positive")
+      event = events.last["_source"]
+      event["error_code"].should eq("1")
+      event["result"].should eq("negative")
+    end
+
   end
 
 
