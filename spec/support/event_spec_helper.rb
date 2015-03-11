7002ad3af80ff60ba5db6b6f6e21f5e2c3cfc8c8
Santiago Palladino <spalladino@gmail.com>
2015-03-11 11:10:18 -0300

    Added comment on hack for fixing failing spec on travis

--- spec/support/event_spec_helper.rb
+++ spec/support/event_spec_helper.rb
@@ -1,3 +1,4 @@
+# Ensure Event class is loaded first from app/models before reopening it to ensure its super class is properly set
 Event; class Event
   def self.create_and_index indexed_fields, params={}
     event = self.make params
