c0cd47f00d008195f48818f85e4bf1e778216486
Santiago Palladino <spalladino@gmail.com>
2015-04-17 19:45:22 -0300

    Load locations from locations service in laboratories form #206

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require select2
 //= require instedd-bootstrap
 //= require_tree .
---
+++ app/assets/javascripts/laboratory.js
@@ -0,0 +1,51 @@
+$(function() {
+
+  var formatLocation = function(location) {
+    var name = location.name;
+    if (location.ancestors.length > 0) {
+      var ancestorsNames = _.pluck(location.ancestors, 'name');
+      name += (" (" + ancestorsNames.reverse().join(", ") + ")");
+    }
+    return {
+      id: location.id,
+      text: name
+    }
+  };
+
+  $('#laboratory_location_geoid').select2({
+    placeholder: "Search locations",
+    minimumInputLength: 2,
+    ajax: {
+      url: "http://localhost:8080/suggest",
+      dataType: 'json',
+      quietMillis: 250,
+      data: function (term, page) {
+        return {
+          name: term,
+          limit: 20,
+          offset: page * 20,
+          ancestors: true
+        };
+      },
+      results: function (data, page) {
+        return {
+          more: data.length == 20,
+          results: _.map(data, formatLocation)
+        };
+      },
+      cache: true
+    },
+    initSelection: function(element, callback) {
+        var id = $(element).val();
+        if (id !== "") {
+          $.ajax("http://localhost:8080/details", {
+            dataType: 'json',
+            data: { id: id, ancestors: true },
+            success: function(data) {
+              callback(formatLocation(data[0]));
+            }
+          });
+        }
+      }
+  });
+});
