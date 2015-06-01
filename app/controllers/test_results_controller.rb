5a6c7d78c990cc1be7bb0b061d95c98b3850a8de
Adrian Romero <aromero@manas.com.ar>
2015-06-01 16:49:12 -0300

    #237 Renamed EventsController -> TestResultsController

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,5 +1,5 @@
-class EventsController < ApplicationController
-  add_breadcrumb 'Results', :events_path
+class TestResultsController < ApplicationController
+  add_breadcrumb 'Results', :test_results_path
 
   def index
     @combo_laboratories = authorize_resource(Laboratory, READ_LABORATORY)
@@ -8,7 +8,7 @@ class EventsController < ApplicationController
     query["laboratory"] = params["laboratory"] if params["laboratory"].present?
     query["condition"] = params["condition"] if params["condition"].present?
 
-    @events = Cdx::Api::Elasticsearch::Query.new(query).execute["events"]
+    @tests = Cdx::Api::Elasticsearch::Query.new(query).execute["events"]
     @institutions = indexed_model Institution, "institution_id"
     @laboratories = indexed_model Laboratory, "laboratory_id"
     @devices = indexed_model Device, "device_uuid", "uuid"
@@ -17,7 +17,7 @@ class EventsController < ApplicationController
   private
 
   def indexed_model(model, es_field, db_field = "id")
-    ids = @events.map { |event| event[es_field] }
+    ids = @tests.map { |test| test[es_field] }
     model.where(db_field => ids).index_by { |model| model[db_field] }
   end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe EventsController do
+describe TestResultsController do
   let(:user) {User.make}
   before(:each) {sign_in user}
 
-  it "should display an empty page when there are no events" do
+  it "should display an empty page when there are no test results" do
     response = get :index
     response.status.should eq(200)
   end
