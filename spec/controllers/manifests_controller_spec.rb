0d708bcf6e0fbf1b21dd4843e02eebf15d814698
Mariano Abel Coca <marianoabelcoca@gmail.com>
2015-06-26 10:59:28 -0300

    Fixed manifest controller spec

--- spec/controllers/manifests_controller_spec.rb
+++ spec/controllers/manifests_controller_spec.rb
@@ -19,39 +19,23 @@ describe ManifestsController do
       json = {"definition" => %{{
         "metadata": {
           "version" : "1.0.0",
-          "api_version" : "1.1.0",
+          "api_version" : "1.2.0",
           "device_models" : ["GX4001"],
           "source" : { "type" : "json" }
         },
         "field_mapping": {
-          "test" : [
-            {
-              "target_field": "assay_name",
-              "source" : {"lookup" : "Test.assay_name"},
-              "core" : true,
-              "type" : "string"
-            },
-            {
-              "target_field" : "test_type",
-              "source" : {
-                "mapping" : [
-                  {"lookup" : "Test.test_type"},
-                  [
-                    {"match" : "*QC*", "output" : "qc"},
-                    {"match" : "*Specimen*", "output" : "specimen"}
-                  ]
-                ]
-              },
-              "core" : true,
-              "type" : "enum",
-              "options" : [
-                "qc",
-                "specimen"
+          "test.assay_name" : {"lookup" : "Test.assay_name"},
+          "test.type" : {
+            "mapping" : [
+              {"lookup" : "Test.test_type"},
+              [
+                {"match" : "*QC*", "output" : "qc"},
+                {"match" : "*Specimen*", "output" : "specimen"}
               ]
-            }
-          ]
+            ]
+          }
         }
-      }} }
+      }}}
       Manifest.count.should eq(0)
       post :create, manifest: json
       Manifest.count.should eq(1)
