2b06d568f7c765febfd2ccec9d15e2406a1a9ebb
Adrian Romero <aromero@manas.com.ar>
2015-06-29 17:00:17 -0300

    Fixed tests in tests controller locations spec

--- spec/controllers/api/tests_controller_locations_spec.rb
+++ spec/controllers/api/tests_controller_locations_spec.rb
@@ -29,25 +29,25 @@ describe Api::TestsController, elasticsearch: true, validate_manifest: false do
       device2 = Device.make institution: institution, laboratories: [laboratory2]
       device3 = Device.make institution: institution, laboratories: [laboratory3]
 
-      DeviceMessage.create_and_process device: device1, plain_text_data: Oj.dump(results:[condition: "flu_a"])
-      DeviceMessage.create_and_process device: device2, plain_text_data: Oj.dump(results:[condition: "flu_b"])
-      DeviceMessage.create_and_process device: device3, plain_text_data: Oj.dump(results:[condition: "mtb"])
+      DeviceMessage.create_and_process device: device1, plain_text_data: Oj.dump(test:{assays:[name: "flu_a"]})
+      DeviceMessage.create_and_process device: device2, plain_text_data: Oj.dump(test:{assays:[name: "flu_b"]})
+      DeviceMessage.create_and_process device: device3, plain_text_data: Oj.dump(test:{assays:[name: "mtb"]})
 
       response = get_updates(location: leaf_location1.geo_id)
 
-      response.first["results"].first["condition"].should eq("flu_a")
+      response.first["test"]["assays"].first["name"].should eq("flu_a")
 
       response = get_updates(location: leaf_location2.geo_id)
 
-      response.first["results"].first["condition"].should eq("flu_b")
+      response.first["test"]["assays"].first["name"].should eq("flu_b")
 
       response = get_updates(location: parent_location.geo_id).sort_by do |test|
-        test["results"].first["condition"]
+        test["test"]["assays"].first["name"]
       end
 
       response.size.should eq(2)
-      response[0]["results"].first["condition"].should eq("flu_a")
-      response[1]["results"].first["condition"].should eq("flu_b")
+      response[0]["test"]["assays"].first["name"].should eq("flu_a")
+      response[1]["test"]["assays"].first["name"].should eq("flu_b")
     end
 
     it "groups by administrative level" do
@@ -55,14 +55,14 @@ describe Api::TestsController, elasticsearch: true, validate_manifest: false do
       device2 = Device.make institution: institution, laboratories: [laboratory2]
       device3 = Device.make institution: institution, laboratories: [laboratory3]
 
-      DeviceMessage.create_and_process device: device1, plain_text_data: Oj.dump(results:[condition: "flu_a"])
-      DeviceMessage.create_and_process device: device2, plain_text_data: Oj.dump(results:[condition: "flu_b"])
-      DeviceMessage.create_and_process device: device3, plain_text_data: Oj.dump(results:[condition: "mtb"])
+      DeviceMessage.create_and_process device: device1, plain_text_data: Oj.dump(test:{assays:[name: "flu_a"]})
+      DeviceMessage.create_and_process device: device2, plain_text_data: Oj.dump(test:{assays:[name: "flu_b"]})
+      DeviceMessage.create_and_process device: device3, plain_text_data: Oj.dump(test:{assays:[name: "mtb"]})
 
       response = get_updates(group_by: {admin_level: 0})
       response.should eq([
-        {"location"=>parent_location.geo_id, "count"=>2},
-        {"location"=>upper_leaf_location.geo_id, "count"=>1}
+        {"location.admin_levels"=>parent_location.geo_id, "count"=>2},
+        {"location.admin_levels"=>upper_leaf_location.geo_id, "count"=>1}
       ])
     end
   end
