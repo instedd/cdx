fe7cb7aef0592e709429e2de37c97969a5810255
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-07-23 18:13:33 -0300

    Fix test results page (after model refactor)

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -9,15 +9,15 @@ class TestResultsController < ApplicationController
     query["condition"] = params["condition"] if params["condition"].present?
 
     @tests = Cdx::Api::Elasticsearch::Query.new(query).execute["tests"]
-    @institutions = indexed_model Institution, "institution_id"
-    @laboratories = indexed_model Laboratory, "laboratory_id"
-    @devices = indexed_model Device, "device_uuid", "uuid"
+    @institutions = indexed_model Institution, ["institution", "id"]
+    @laboratories = indexed_model Laboratory, ["laboratory", "id"]
+    @devices = indexed_model Device, ["device", "uuid"], "uuid"
   end
 
   private
 
   def indexed_model(model, es_field, db_field = "id")
-    ids = @tests.map { |test| test[es_field] }
+    ids = @tests.map { |test| es_field.inject(test) { |obj, field| obj[field] } }.uniq
     model.where(db_field => ids).index_by { |model| model[db_field] }
   end
 end
