a6b7192920f80ffb4e2940aec3df7cb6721351e0
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-07-27 16:44:21 -0300

    Removed duplicated code for new manifest message

--- spec/support/manifest_spec_helper.rb
+++ spec/support/manifest_spec_helper.rb
@@ -17,12 +17,7 @@ module ManifestSpecHelper
   def assert_manifest_application(mappings_json, custom_json, format_data, expected_fields={}, device= nil)
     format_data = {json: format_data} unless format_data.kind_of?(Hash)
 
-    expected = {
-      "test" =>    { "indexed" => {}, "custom" => {}, "pii" => {} },
-      "sample" =>  { "indexed" => {}, "custom" => {}, "pii" => {} },
-      "patient" => { "indexed" => {}, "custom" => {}, "pii" => {} },
-      "device" =>  { "indexed" => {}, "custom" => {}, "pii" => {} }
-    }.deep_merge(expected_fields)
+    expected = Manifest.new_message.deep_merge(expected_fields)
 
     format_data.each do |format, data|
       manifest = manifest_from_json_mappings(mappings_json, custom_json, format)
