aef183ad53e8254cdfcf7c041d2e2f655d32be8d
Juan Wajnerman <jwajnerman@manas.com.ar>
2015-07-30 17:08:25 -0300

    Use custom range keys for durations

--- lib/cdx/api/elasticsearch/grouping/duration_grouping_detail.rb
+++ lib/cdx/api/elasticsearch/grouping/duration_grouping_detail.rb
@@ -7,14 +7,14 @@ class DurationGroupingDetail < GroupingDetail
     raise "Can't group by duration field without ranges" unless ranges
     super name, field_definition, uri_param
     @ranges = ranges
-    @number_range_to_components = {}
   end
 
   def to_es
     {
       count: {
         range: {
-          field: "#{field_definition.name}.in_millis", ranges: ranges_to_es
+          field: "#{field_definition.name}.in_millis",
+          ranges: ranges_to_es
         }
       }
     }
@@ -23,13 +23,13 @@ class DurationGroupingDetail < GroupingDetail
   def ranges_to_es
     ranges.map do |range|
       range_hash = Cdx::Field::DurationField.parse_range range
-      @number_range_to_components["#{range_hash["from"]}||#{range_hash["to"]}"] = range
+      range_hash["key"] = range
       range_hash
     end
   end
 
   def yield_bucket(bucket)
-    {uri_param => @number_range_to_components["#{normalize(bucket["from"])}||#{normalize(bucket["to"])}"]}
+    {uri_param => bucket["key"]}
   end
 
   def normalize(value)
