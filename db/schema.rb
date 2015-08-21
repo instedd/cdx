dcb939d99420bee84b7be9d378602475890dbfcc
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-08-21 17:09:19 -0300

    Added missing schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818200013) do
+ActiveRecord::Schema.define(version: 20150821200128) do
 
   create_table "activation_tokens", force: :cascade do |t|
     t.string   "value",      limit: 255
