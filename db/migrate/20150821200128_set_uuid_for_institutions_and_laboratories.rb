2ed879dd243c4e3fa463f9d516e80bdafb7795e6
Ary Borenszweig <aborenszweig@manas.com.ar>
2015-08-21 17:03:31 -0300

    Migration to set uuid to institutions and laboratories

---
+++ db/migrate/20150821200128_set_uuid_for_institutions_and_laboratories.rb
@@ -0,0 +1,19 @@
+class SetUuidForInstitutionsAndLaboratories < ActiveRecord::Migration
+  class Institution < ActiveRecord::Base; end
+  class Laboratory < ActiveRecord::Base; end
+
+  def up
+    Institution.find_each do |x|
+      x.uuid ||= Guid.new.to_s
+      x.save!
+    end
+
+    Laboratory.find_each do |x|
+      x.uuid ||= Guid.new.to_s
+      x.save!
+    end
+  end
+
+  def down
+  end
+end
