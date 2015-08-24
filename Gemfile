c177f9a2422d94c8e3120e57d2692a8addebf007
Mariano Abel Coca <marianoabelcoca@gmail.com>
2015-08-24 15:25:27 -0300

    Fixed #273. Removed instedd-bootstrap.

--- Gemfile
+++ Gemfile
@@ -15,11 +15,7 @@ gem 'nokogiri'
 
 gem 'foreman'
 
-# A newer version breaks instedd-bootstrap
-gem 'bootstrap-sass', '2.3.2.1'
-
 gem 'poirot_rails', git: 'https://github.com/instedd/poirot_rails.git', branch: 'master'
-gem 'instedd-bootstrap', git: "https://bitbucket.org/instedd/instedd-bootstrap.git", branch: 'master'
 gem 'ruby-openid'
 gem 'alto_guisso_rails', github: "instedd/alto_guisso_rails", branch: 'master'
 gem 'alto_guisso', github: "instedd/alto_guisso", branch: 'master'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,24 @@ module ApplicationHelper
   def check_access(resource, action)
     Policy.authorize action, resource, current_user, @current_user_policies
   end
+
+  def flash_message
+    res = nil
+
+    keys = { :notice => 'flash_notice', :error => 'flash_error', :alert => 'flash_error' }
+
+    keys.each do |key, value|
+      if flash[key]
+        html_option = { :class => "flash #{value}" }
+        html_option[:'data-hide-timeout'] = 3000 if key == :notice
+        res = content_tag :div, html_option do
+          content_tag :div do
+            flash[key]
+          end
+        end
+      end
+    end
+
+    res
+  end
 end
