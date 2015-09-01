4f4c3013d20739289b61b194fb77f15368fde43b
Mariano Abel Coca <marianoabelcoca@gmail.com>
2015-09-01 15:26:39 -0300

    Fixed #301. Added more filters to tests screen.

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,14 +1,25 @@
 class TestResultsController < ApplicationController
   def index
-    @combo_laboratories = check_access(Laboratory, READ_LABORATORY)
+    @laboratories = check_access(Laboratory, QUERY_TEST)
+    @institutions = check_access(Institution, QUERY_TEST)
+    @devices = check_access(Device, QUERY_TEST)
+    @results = Cdx.core_fields.find { |field| field.name == 'result' }.options
+    @conditions = Condition.all.map &:name
+    @date_options = [["Previous month", 1.month.ago.beginning_of_month], ["Previous week", 1.week.ago.beginning_of_week],["Previous year", 1.year.ago.beginning_of_year]]
 
     @page_size = (params["page_size"] || 10).to_i
     @page = (params["page"] || 1).to_i
     offset = (@page - 1) * @page_size
 
     @filter = {}
-    @filter["laboratory.id"] = params["laboratory.id"] if params["laboratory.id"].present?
+    @filter["institution.uuid"] = params["institution.uuid"] if params["institution.uuid"].present?
+    @filter["laboratory.uuid"] = params["laboratory.uuid"] if params["laboratory.uuid"].present?
     @filter["test.assays.condition"] = params["test.assays.condition"] if params["test.assays.condition"].present?
+    @filter["device.uuid"] = params["device.uuid"] if params["device.uuid"].present?
+    @filter["test.assays.condition"] = params["test.assays.condition"] if params["test.assays.condition"].present?
+    @filter["test.assays.result"] = params["test.assays.result"] if params["test.assays.result"].present?
+    @filter["sample.id"] = params["sample.id"] if params["sample.id"].present?
+    @filter["since"] = params["since"] if params["since"].present?
 
     @query = @filter.dup
     @query["page_size"] = @page_size
