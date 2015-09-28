9f517ebe9511e8b45b39411851ddda1a179d2d86
Santiago Palladino <spalladino@gmail.com>
2015-09-28 17:28:48 -0300

    Added auth checks to device models controller, removing decent exposure in the process. Added specs for all actions. #327 #329

--- app/controllers/device_models_controller.rb
+++ app/controllers/device_models_controller.rb
@@ -1,46 +1,65 @@
 class DeviceModelsController < ApplicationController
-  expose(:device_models) { DeviceModel.includes(:manifest) }
-  expose(:device_model, attributes: :device_model_params)
+
+  before_filter :load_institutions,  except: [:index, :destroy]
+
   before_filter do
     @main_column_width = 6 unless params[:action] == 'index'
   end
 
+  def index
+    @device_models = authorize_resource(DeviceModel, READ_DEVICE_MODEL) or return
+    @device_models = @device_models.includes(:manifest)
+  end
+
+  def show
+    @device_model = authorize_resource(DeviceModel.find(params[:id]), READ_DEVICE_MODEL) or return
+    @manifest = @device_model.current_manifest
+  end
+
+  def new
+    authorize_resource(Institution, REGISTER_INSTITUTION_DEVICE_MODEL) or return
+
+    @device_model = DeviceModel.new
+    @device_model.manifest = Manifest.new
+    @device_model.institution = @institutions.first if @institutions.one?
+  end
+
   def create
+    authorize_resource(Institution.find(device_model_create_params[:institution_id]), REGISTER_INSTITUTION_DEVICE_MODEL) or return
+    @device_model = DeviceModel.new(device_model_create_params)
+
     respond_to do |format|
-      if device_model.save
+      if @device_model.save
         format.html { redirect_to device_models_path, notice: 'Device Model was successfully created.' }
-        format.json { render action: 'show', status: :created, device_model: device_model }
+        format.json { render action: 'show', status: :created, device_model: @device_model }
       else
         format.html { render action: 'new' }
-        format.json { render json: device_model.errors, status: :unprocessable_entity }
+        format.json { render json: @device_model.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def new
-    device_model.manifest = Manifest.new
-  end
-
-  def show
-  end
-
   def edit
+    @device_model = authorize_resource(DeviceModel.find(params[:id]), UPDATE_DEVICE_MODEL)
   end
 
   def update
+    @device_model = authorize_resource(DeviceModel.find(params[:id]), UPDATE_DEVICE_MODEL) or return
+
     respond_to do |format|
-      if device_model.update(device_model_params)
+      if @device_model.update(device_model_update_params)
         format.html { redirect_to device_models_path, notice: 'Device Model was successfully updated.' }
-        format.json { render action: 'show', status: :created, device_model: device_model }
+        format.json { render action: 'show', status: :created, device_model: @device_model }
       else
         format.html { render action: 'new' }
-        format.json { render json: device_model.errors, status: :unprocessable_entity }
+        format.json { render json: @device_model.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    device_model.destroy
+    @device_model = authorize_resource(DeviceModel.find(params[:id]), DELETE_DEVICE_MODEL) or return
+    @device_model.destroy!
 
     respond_to do |format|
       format.html { redirect_to device_models_path }
@@ -50,7 +69,15 @@ class DeviceModelsController < ApplicationController
 
   private
 
-  def device_model_params
+  def load_institutions
+    @institutions = authorize_resource(Institution, REGISTER_INSTITUTION_DEVICE_MODEL)
+  end
+
+  def device_model_create_params
+    params.require(:device_model).permit(:name, :institution_id, manifest_attributes: [:definition])
+  end
+
+  def device_model_update_params
     params.require(:device_model).permit(:name, manifest_attributes: [:definition])
   end
 end
