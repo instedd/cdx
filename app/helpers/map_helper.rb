ff6fc566aabb417c5c8dc2ee5235d958f99ce47e
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-09-29 19:26:08 -0300

    map_tag for showing simple map pictures
    For production use, we should create a project at
    https://developers.google.com/maps/documentation/static-maps/
    
    The API key should be specified in either the `google_maps_api_key`
    key of the settings.yml, or at the `GOOGLE_MAPS_API_KEY` environment
    variable.
    
    See #362

---
+++ app/helpers/map_helper.rb
@@ -0,0 +1,11 @@
+module MapHelper
+  def map_tag lat, lng, zoom=9, width=180, height=178
+    image_tag "http://maps.googleapis.com/maps/api/staticmap?center=#{lat},#{lng}&zoom=#{zoom}&size=#{width}x#{height}&style=feature:landscape|element:all|saturation:-100&style=feature:transit|element:all|saturation:-100&style=feature:landscape|element:all|saturation:-100&style=feature:poi|element:all|saturation:-100&style=feature:water|saturation:-100|invert_lightness:true&style=feature:road|element:all|saturation:-100&markers=color:red|#{lat},#{lng}#{"&key=#{google_maps_api_key}" if google_maps_api_key}"
+  end
+
+  private
+
+  def google_maps_api_key
+    ENV['GOOGLE_MAPS_API_KEY'] || Settings.google_maps_api_key
+  end
+end
