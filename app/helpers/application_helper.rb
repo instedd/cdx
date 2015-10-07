dd6b49feec9a408baa521951cd72ed39ade88256
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-10-07 18:10:48 -0300

    cdx_tabs component helper without interaction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,6 +30,23 @@ module ApplicationHelper
   define_component :card, sections: [:top, :actions, :bottom], attributes: [:image]
   define_component :cdx_table, sections: [:thead, :tbody], attributes: [:title]
 
+  define_component :cdx_tabs do |c|
+    c.section :headers, multiple: true, component: :cdx_tabs_header
+    c.section :contents, multiple: true
+  end
+  define_component :cdx_tabs_header, attributes: [:title]
+  ViewComponents::ComponentsBuilder::Component.classes[:cdx_tabs].class_eval do
+    def tab(options, &block)
+      self.header options
+      if block_given?
+        self.content &block
+      else
+        self.content do
+        end
+      end
+    end
+  end
+
   def test_results_table(attributes)
     options = { filter: {} }
     options[:filter]['laboratory.uuid'] = attributes[:filter][:laboratory].uuid if attributes[:filter][:laboratory]
