1b0bf935e2a9796ef1015597b73a2d44f214e000
Matias Garcia Isaia <mgarcia@manas.com.ar>
2015-10-07 15:51:42 -0300

    Display correct page number on change
    See #362

--- app/assets/javascripts/components/pager.js.jsx
+++ app/assets/javascripts/components/pager.js.jsx
@@ -23,27 +23,34 @@ var Pager = React.createClass({
     }, 500);
   },
 
-  fireShowPage: function() {
+  _computedPage: function() {
     var pageNumber = parseInt(this.state.page) || 1;
-    pageNumber = Math.max(Math.min(pageNumber, this.props.totalPages), 1)
+    return Math.max(Math.min(pageNumber, this.props.totalPages), 1);
+  },
+
+  fireShowPage: function() {
+    var pageNumber = this._computedPage();
     this.props.showPage(pageNumber);
+    this.setState(React.addons.update(this.state, {
+      page: { $set: pageNumber },
+    }));
   },
 
   prevPage: function(event) {
-    this._setPage(parseInt(this.state.page) - 1);
+    this._setPage(parseInt(this._computedPage()) - 1);
     event.preventDefault();
   },
 
   nextPage: function(event) {
-    this._setPage(parseInt(this.state.page) + 1);
+    this._setPage(parseInt(this._computedPage()) + 1);
     event.preventDefault();
   },
 
   render: function() {
     return (
       <div className="pagination">
-        <a className="btn-link" href="#" onClick={this.prevPage} disabled={this.state.page == 1 ? "disabled" : ""}>&lt;</a>
-        <a className="btn-link" href="#" onClick={this.nextPage} disabled={this.state.page == this.props.totalPages ? "disabled" : ""}>&gt;</a>
+        <a className="btn-link" href="#" onClick={this.prevPage} disabled={this._computedPage() <= 1 ? "disabled" : ""}>&lt;</a>
+        <a className="btn-link" href="#" onClick={this.nextPage} disabled={this._computedPage() >= this.props.totalPages ? "disabled" : ""}>&gt;</a>
         <input className="input-x-small text-right" type="text" value={this.state.page} onChange={this.handlePageChange}/>
         <span>
           of {this.props.totalPages}
