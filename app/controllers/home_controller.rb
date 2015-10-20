e3c0121bc05effdbeb958da6d7aab1425a50eab3
Martin Scebba <mscebba@manas.com.ar>
2015-10-20 15:42:32 -0300

    Confirmation screens: verify, join, invitation.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,18 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
-  layout 'messages', only: :verify
-
   def index
   end
 
   def verify
+    render layout: "messages"
+  end
+
+  def confirm
+    render layout: "clean"
+  end
+
+  def join
+    render layout: "clean"
   end
 end
