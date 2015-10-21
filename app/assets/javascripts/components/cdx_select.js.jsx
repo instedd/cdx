9c3b9f66d319bd8c3d82dc1d868e4634ad03a996
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-10-21 17:44:50 -0300

    replace dropdown in test_results/index page allow cdx_select to be used without rails form builder allos cdx_select to use first element as placeholder if empty value is provided

--- app/assets/javascripts/components/cdx_select.js.jsx
+++ app/assets/javascripts/components/cdx_select.js.jsx
@@ -6,10 +6,16 @@ var CdxSelect = React.createClass({
   },
 
   render: function() {
+    var placeholder = "Select..."
+    if (this.props.items.length > 0 && this.props.items[0].value === "") {
+      placeholder = this.props.items[0].label;
+    }
+
     return (<Select className={this.props.className}
       name={this.props.name}
       value={this.props.value}
       options={this.props.items}
+      placeholder={placeholder}
       clearable={false}>
     </Select>);
   }
