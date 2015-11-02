955e291a909800c227ec99afcf853384046d2717
Anthony O’Leary <anthony.oleary@finddx.org>
2015-11-02 15:34:38 +0000

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,39 @@ ActiveRecord::Schema.define(version: 20151103193538) do
 
   add_index "activations", ["activation_token_id"], name: "index_activations_on_activation_token_id", unique: true, using: :btree
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace",     limit: 255
+    t.text     "body",          limit: 65535
+    t.string   "resource_id",   limit: 255,   null: false
+    t.string   "resource_type", limit: 255,   null: false
+    t.integer  "author_id",     limit: 4
+    t.string   "author_type",   limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
   create_table "computed_policies", force: :cascade do |t|
     t.integer "user_id",                  limit: 4
     t.string  "action",                   limit: 255
@@ -368,10 +401,19 @@ ActiveRecord::Schema.define(version: 20151103193538) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "isActive",                                   default: true
+    t.boolean  "isArchived"
+    t.boolean  "isAdmin",                                    default: false
+    t.boolean  "is_admin",                                   default: false
+    t.boolean  "is_active",                                  default: true
+    t.boolean  "is_archived",                                default: false
     t.datetime "password_changed_at"
     t.string   "locale",                         limit: 255, default: "en"
     t.boolean  "timestamps_in_device_time_zone",             default: false
     t.string   "time_zone",                      limit: 255, default: "UTC"
+    t.datetime "deleted_at"
+    t.boolean  "archived",                                   default: false
+    t.boolean  "active",                                     default: true
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
