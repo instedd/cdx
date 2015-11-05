6ddcac87db98f0a54aeba7d7bc7fdfc9d04c2bb5
Martin Scebba <mscebba@manas.com.ar>
2015-11-05 18:07:48 -0300

    Fixes #475 - Fix dashboard width

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,11 @@
 class DashboardsController < ApplicationController
+  before_filter :set_width
+
   def nndd
     return unless authorize_resource(TestResult, MEDICAL_DASHBOARD)
+  end
+
+  def set_width
+    @main_column_width = 10
   end
 end
