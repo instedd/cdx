efafe96d8d9898e93c3469f57f1b660a0b1d2845
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-11-05 14:44:32 -0300

    configure phantomjs-2.0.0 for travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'webmock/rspec'
 require 'capybara-screenshot/rspec'
+
 Capybara.javascript_driver = :poltergeist
 
 # HTTPI.log = false
