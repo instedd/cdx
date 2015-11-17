331836d6475e570c405239de493b62c8a08469bf
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-11-17 14:06:22 -0300

    perform client side search over site name. case/deburr insensitive string matching

--- app/assets/javascripts/components/sites.js.jsx
+++ app/assets/javascripts/components/sites.js.jsx
@@ -31,14 +31,14 @@ var SitePicker = React.createClass({
   },
 
   getInitialState: function() {
-    return { sites: [], sites_tree: [], selected_site: null };
+    return { sites: [], sites_tree: [], selected_site: null, query: '' };
   },
 
   componentDidMount: function() {
     $.get(this.props.url, function(result) {
       if (!this.isMounted()) return;
 
-      roots_and_selected = this._buildTree(result.sites) || this.state.selected_site;
+      roots_and_selected = this._buildTree(result.sites, '', this.props.selected_uuid);
 
       this.setState(React.addons.update(this.state, {
         sites: { $set: result.sites },
@@ -48,21 +48,36 @@ var SitePicker = React.createClass({
     }.bind(this));
   },
 
-  _buildTree: function(sites) {
+  _siteMatch: function(site, query) {
+    return _.deburr(site.name.toLowerCase()).indexOf(query) != -1;
+  },
+
+  _buildTree: function(sites, query, selected_uuid) {
     var sites_by_uuid = {};
     var roots = [];
     var selected = null;
+    var matched_sites = [];
+    query = _.deburr(query).toLowerCase();
 
+    // prepares a matched_sites with all sites that match query
+    // but it prepares a children property on each site for
+    // building later the tree
     _.each(sites, function(site){
+      if (query != '' && !this._siteMatch(site, query)) return;
+      matched_sites.push(site);
       sites_by_uuid[site.uuid] = site;
       site.children = [];
-      site.selected = site.uuid == this.props.selected_uuid;
+      site.selected = site.uuid == selected_uuid;
       if (site.selected) {
         selected = site;
       }
     }.bind(this));
 
-    _.each(sites, function(site) {
+    // builds a tree with all matched_sites and with the children
+    // information. If parent is not present, it is considered a root.
+    // Roots may vary depending on the query argument due to non-match of parents.
+    // TODO what should happen in Match1 -> NonMatch1 -> Match2 ?
+    _.each(matched_sites, function(site) {
       parent = sites_by_uuid[site.parent_uuid]
       if (parent) {
         parent.children.push(site)
@@ -87,10 +102,33 @@ var SitePicker = React.createClass({
     this.props.onSiteSelected(site);
   },
 
+  // debounce: http://stackoverflow.com/a/24679479/30948
+  componentWillMount: function() {
+    this.handleSearchDebounced = _.debounce(function(){
+      this.handleSearch.apply(this, [this.state.query]);
+    }, 500);
+  },
+
+  onSearchChange: function(event) {
+    this.setState(React.addons.update(this.state, {
+      query: { $set: event.target.value }
+    }));
+    this.handleSearchDebounced();
+  },
+
+  handleSearch: function(query) {
+    roots_and_selected = this._buildTree(this.state.sites, query, this.state.selected_site.uuid);
+    // should not change selected site while filtering
+
+    this.setState(React.addons.update(this.state, {
+      sites_tree: { $set: roots_and_selected[0] },
+    }));
+  },
+
   render: function() {
     return (
       <div>
-        <input type="text" onChange={this.onChange} autoFocus="true" />
+        <input type="text" onChange={this.onSearchChange} autoFocus="true" />
         <SitesTreeView sites={this.state.sites_tree} onSiteClick={this.selectSite} />
       </div>
     )
