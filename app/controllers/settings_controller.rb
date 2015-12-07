6fbea633c7aada38efa5e105c212b426dddc9f83
Stewart Campbell <stewartcuk@gmail.com>
2015-12-07 13:44:17 +0000

    Adds SettingsController
    Refactor settings and move from userController into it's own
    SettingsController

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,27 @@
+class SettingsController < ApplicationController
+  before_filter :load_locales
+
+  def edit; end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to settings_path, notice: I18n.t('user.settings.updated')
+    else
+      render :settings
+    end
+  end
+
+  private
+
+  def load_locales
+    @locales ||= [%w(English en)]
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :locale,
+      :time_zone,
+      :timestamps_in_device_time_zone
+    )
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_filter :ensure_context
-  LOCALES = [
-              ["English", "en"],
-            ]
-
   def edit
     @user = User.find(params[:id])
   end
@@ -17,21 +12,6 @@ class UsersController < ApplicationController
 
     redirect_to edit_user_path(@user), notice: @_message
   end
-
-  def settings
-    load_locales
-  end
-
-  def update_settings
-    params = user_params
-    if current_user.update(params)
-      redirect_to settings_path, notice: "Settings updated"
-    else
-      load_locales
-      render :settings
-    end
-  end
-
   private
 
   def admin_update_params
@@ -40,13 +20,5 @@ class UsersController < ApplicationController
 
   def can_edit?
     current_user.id != params[:id].to_i
-  end
-
-  def user_params
-    params.require(:user).permit(:locale, :time_zone, :timestamps_in_device_time_zone)
-  end
-
-  def load_locales
-    @locales = LOCALES
   end
 end
