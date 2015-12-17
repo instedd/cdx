a26fdac474de6bfd7f567d51afc5e825cb5339d3
Santiago Palladino <spalladino@gmail.com>
2015-12-17 20:03:24 -0300

    Restored login page object for cucumber tests

---
+++ features/support/page_objects/login_page.rb
@@ -0,0 +1,9 @@
+class LoginPage < SitePrism::Page
+  set_url '/users/sign_in'
+
+  section 'form', '#new_user' do
+    element :user_name, '#user_email'
+    element :password, '#user_password'
+    element :login, '[name="commit"]'
+  end
+end
