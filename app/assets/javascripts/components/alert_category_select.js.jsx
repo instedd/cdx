4a2e21fa4ac459c9fb2b55c4dbcb53bf18df02df
Anthony O’Leary <anthony.oleary@finddx.org>
2015-12-30 15:52:31 +0000

    code format indent

--- app/assets/javascripts/components/alert_category_select.js.jsx
+++ app/assets/javascripts/components/alert_category_select.js.jsx
@@ -2,157 +2,139 @@
 
 var AlertCategorySelect = React.createClass({
 	mixins: [React.addons.LinkedStateMixin],
-		componentDidMount: function(){
-			document.getElementById('errorcoderow').style.display = 'none';
-			document.getElementById('anomalierow').style.display = 'none';
-				},
+	componentDidMount: function(){
+		document.getElementById('errorcoderow').style.display = 'none';
+		document.getElementById('anomalierow').style.display = 'none';
+	},
 	getInitialState: function() {
 		category_keys = Object.keys(this.props.category_types);
-		current_category = category_keys[1];
-		alert1={ name1: "aza", age: 252};
-    return {
-      current_category: category_keys[1], alert1:{ name1: "aa", age: 22}, nameField:"", siteField:"", errorCodeField:"", anomalieField:""
-    };
-  },
- handleNameChange: function(e) {
-    this.setState({nameField: e.target.value});
+		return {
+			// had issues with a structure such as alert1 -updating the data object field in the 'select' options library so used individual fields
+			current_category: category_keys[1], alert1:{ name1: "aa"}, nameField:"", siteField:"", errorCodeField:"", anomalieField:""
+		};
+	},
+	// handleNameChange: function(e) {
+		//    this.setState({nameField: e.target.value});
+		//  },
+		categoryChanged: function (e) {
 
-//   alert1.name1 = e.target.value;
-//	this.setState(alert1);
+			document.getElementById('errorcoderow').style.display = 'none';
+			document.getElementById('anomalierow').style.display = 'none';
 
-  },
-/*
- handleSiteChange: function(val) {
-    this.setState({siteField: val});
-  },
- handleErrorCodeChange: function(val) {
-    this.setState({errorCodeField: val});
-  },
-*/
-  categoryChanged: function (e) {
-	
-	document.getElementById('errorcoderow').style.display = 'none';
-	document.getElementById('anomalierow').style.display = 'none';
-	
-//	var e1 = document.getElementById('xxx')
- //   e1.style.display = (e1.style.display == 'block') ? 'none' : 'block';
+			//	var e1 = document.getElementById('xxx')
+			//   e1.style.display = (e1.style.display == 'block') ? 'none' : 'block';
 
-if (e.currentTarget.value=='device_errors')
-   	var e1 = document.getElementById('errorcoderow')
-    e1.style.display = 'block';
-end
+			if (e.currentTarget.value=='device_errors')
+			var e1 = document.getElementById('errorcoderow')
+			e1.style.display = 'block';
+			end
 
-if (e.currentTarget.value=='anomalies')
-   	var e1 = document.getElementById('anomalierow')
-    e1.style.display = 'block';
-end
+			if (e.currentTarget.value=='anomalies')
+			var e1 = document.getElementById('anomalierow')
+			e1.style.display = 'block';
+			end
 
-	
-    this.setState({
-      current_category: e.currentTarget.value
-      });
+			this.setState({
+				current_category: e.currentTarget.value
+			});
+
+		},
+		handleAlertSubmit: function(comment) {
+			var current_category = this.state.current_category;
+
+			//check using: https://github.com/github/fetch
+			//http://voidcanvas.com/react-tutorial-two-way-data-binding/
+
+			this.setState({category1: current_category});
 
 
-     
+			alert={ name: this.state.nameField, sites_info: this.state.siteField, error_code: this.state.errorCodeField};
+			$.ajax({
+				url: this.props.url,
+				dataType: 'json',
+				type: 'POST',
+				data: {"category": this.state.current_category, "alert" : alert},
+				success: function(data) {
+					//   this.loadCommentsFromServer();
+				}.bind(this),
+				error: function(xhr, status, err) {
+					console.error(this.props.url, status, err.toString());
+				}.bind(this)
+			});
+		},
+		render: function() {
+			return (
+				<div>
+				<form className="commentForm" onSubmit={this.handleAlertSubmit}>
 
+				<div className="row">
+				<div className="col pe-2">
+				Name
+				</div>
+				<div className="col pe-2">
+				<input type="text" placeholder="Title" name="alert[name]"  valueLink={this.linkState('nameField')}  />
+				</div>
+				</div>   
 
-  },
-handleAlertSubmit: function(comment) {
-    var current_category = this.state.current_category;
+				<AlertDescription />
 
-  //check using: https://github.com/github/fetch
-  //http://voidcanvas.com/react-tutorial-two-way-data-binding/
+				<div className="row">
+				<div className="col pe-2">
+				&nbsp;
+				</div>
+				<div className="col">
+				<input type="radio" name="category_type" value={category_keys[0]}  onChange={this.categoryChanged} />Anomalies
+				</div>
+				</div>
+				<div className="row">
+				<div className="col pe-2">
+				&nbsp;
+				</div>
+				<div className="col">
+				<input type="radio" name="category_type" value={category_keys[1]} onChange={this.categoryChanged} />Device Errors
+				</div>
+				</div>
 
-this.setState({category1: current_category});
-
-
-alert2={ name: this.state.nameField, age: 252, sites_info: this.state.siteField, error_code: this.state.errorCodeField};
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: {"category": this.state.current_category, "alert" : alert2},
-      success: function(data) {
-     //   this.loadCommentsFromServer();
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  render: function() {
-    return (
-      	<div>
-     <form className="commentForm" onSubmit={this.handleAlertSubmit}>
-
-	        <div className="row">
-	          <div className="col pe-2">
-	            Name
-	          </div>
-	          <div className="col pe-2">
-	            <input type="text" placeholder="Title" name="alert[name]"  onChange={this.handleNameChange}   />
-	          </div>
-			 </div>   
-		
-		    <AlertDescription />
-	
-			<div className="row">
-	          <div className="col pe-2">
-	            &nbsp;
-	          </div>
-	          <div className="col">
-			<input type="radio" name="category_type" value={category_keys[0]}  onChange={this.categoryChanged} />Anomalies
-			 </div>
-			</div>
-			<div className="row">
-	          <div className="col pe-2">
-	            &nbsp;
-	          </div>
-	          <div className="col">
-			<input type="radio" name="category_type" value={category_keys[1]} onChange={this.categoryChanged} />Device Errors
-			 </div>
-	        </div>
-	
-	        	<div className="row">
-		          <div className="col pe-2">
-		            &nbsp;
-		          </div>
-		          <div className="col">
+				<div className="row">
+				<div className="col pe-2">
+				&nbsp;
+				</div>
+				<div className="col">
 				<input type="radio" name="category_type" value={category_keys[2]} onChange={this.categoryChanged} />Quality Assurance
-				 </div>
-		        </div>
-	        
-	
-				<div className="row">
-				  <div className="col pe-2">
-	             &nbsp;
-	            </div>
-			 <div className="col">
-			<input type="radio" name="category_type" value={category_keys[3]} onChange={this.categoryChanged} />Test Results
-			 </div>
-			 </div>
-
-
-   <AlertSite sites={this.props.sites}  valueLink={this.linkState('siteField')} />
-
-   <AlertErrorCode valueLink={this.linkState('errorCodeField')} />
-
-    <AlertAnomalieType anomalie_types={this.props.anomalie_types}  valueLink={this.linkState('anomalieField')} />
+				</div>
+				</div>
 
 
 				<div className="row">
-		          <div className="col pe-2">
-		            &nbsp;
-		          </div>
-		          <div className="col">
-		             <input type="submit" value="Create Alert" className="btn-primary"/>
-		          </div>
-		
-		        </div>
-	
-	 </form>
-	    </div>
-    );
-  }
-});
+				<div className="col pe-2">
+				&nbsp;
+				</div>
+				<div className="col">
+				<input type="radio" name="category_type" value={category_keys[3]} onChange={this.categoryChanged} />Test Results
+				</div>
+				</div>
+
+
+				<AlertSite sites={this.props.sites}  valueLink={this.linkState('siteField')} />
+
+				<AlertErrorCode valueLink={this.linkState('errorCodeField')} />
+
+				<AlertAnomalieType anomalie_types={this.props.anomalie_types}  valueLink={this.linkState('anomalieField')} />
+
+
+				<div className="row">
+				<div className="col pe-2">
+				&nbsp;
+				</div>
+				<div className="col">
+				<input type="submit" value="Create Alert" className="btn-primary"/>
+				</div>
+
+				</div>
+
+				</form>
+				</div>
+			);
+		}
+	});
 
--- app/assets/javascripts/components/test_hide.js.jsx
+++ app/assets/javascripts/components/test_hide.js.jsx
@@ -1,51 +1,51 @@
 
 var TestHide = React.createClass({
-  render: function() {
-    return (
-	     <div className="row" id="xxx">
-	      <div className="col pe-2">
-	        Test hide
-	      </div>
-	    <div className="col">
-		   <input type="text" placeholder="YYYYour name" ref="author" />
-	    <input type="text" placeholder="SSSay something..." ref="comment" />
-	      </div>
-	</div>
-    );
-  }
+	render: function() {
+		return (
+			<div className="row" id="xxx">
+			<div className="col pe-2">
+			Test hide
+			</div>
+			<div className="col">
+			<input type="text" placeholder="YYYYour name" ref="author" />
+			<input type="text" placeholder="SSSay something..." ref="comment" />
+			</div>
+			</div>
+		);
+	}
 });
 
 
 
 var AlertDescription = React.createClass({
-  render: function() {
-    return (
-	     <div className="row">
-	      <div className="col pe-2">
-	        Description
-	      </div>
-	    <div className="col">
-		   <input type="text" placeholder="description" ref="alert=description" />
-	      </div>
-	</div>
-    );
-  }
+	render: function() {
+		return (
+			<div className="row">
+			<div className="col pe-2">
+			Description
+			</div>
+			<div className="col">
+			<input type="text" placeholder="description" ref="alert=description" />
+			</div>
+			</div>
+		);
+	}
 });
 
 
 var AlertErrorCode = React.createClass({
-  render: function() {
-    return (
-	     <div className="row" id="errorcoderow">
-	      <div className="col pe-2">
-	        Errors
-	      </div>
-	    <div className="col">
-		   <input type="text" placeholder="All error codes will be reported" valueLink={this.props.valueLink} />
-	      </div>
-	</div>
-    );
-  }
+	render: function() {
+		return (
+			<div className="row" id="errorcoderow">
+			<div className="col pe-2">
+			Errors
+			</div>
+			<div className="col">
+			<input type="text" placeholder="All error codes will be reported" valueLink={this.props.valueLink} />
+			</div>
+			</div>
+		);
+	}
 });
 
 
@@ -53,44 +53,44 @@ var AlertErrorCode = React.createClass({
 
 // http://voidcanvas.com/react-tutorial-two-way-data-binding/
 var AlertSite = React.createClass({	
-		getDefaultProps: function(){
-					return {
-						multiple: true,
-						name: 'selectSite',
-					}
-				},					
-	                  //https://github.com/JedWatson/react-select/issues/256				
-					  onChange(textValue, arrayValue) {					  
-					      // Multi select: grab values and send array of values to valueLink
-					     this.props.valueLink.requestChange(_.pluck(arrayValue, 'value').join());
-					  },
-  render: function() {
-	var siteOptions=[];
-    for (var i = 0; i < this.props.sites.length; i++) {
-		siteOption={};
-	    siteOption["value"] = this.props.sites[i].id;
-	    siteOption["label"] = this.props.sites[i].name;
-	    siteOptions.push(siteOption);
-	}
-	
-	var { valueLink, value, onChange, ...other } = this.props;
-    return (
-	     <div className="row">
-	      <div className="col pe-2">
-	        &nbsp;
-	      </div>
-	    <div className="col">
-		<Select
-			    name="site"
-			    value={value || valueLink.value}
-			    options={siteOptions}			   
-			    multi="true"			    
-			    onChange={this.onChange}
+	getDefaultProps: function(){
+		return {
+			multiple: true,
+			name: 'selectSite',
+		}
+	},					
+	//https://github.com/JedWatson/react-select/issues/256				
+	onChange(textValue, arrayValue) {					  
+		// Multi select: grab values and send array of values to valueLink
+		this.props.valueLink.requestChange(_.pluck(arrayValue, 'value').join());
+	},
+	render: function() {
+		var siteOptions=[];
+		for (var i = 0; i < this.props.sites.length; i++) {
+			siteOption={};
+			siteOption["value"] = this.props.sites[i].id;
+			siteOption["label"] = this.props.sites[i].name;
+			siteOptions.push(siteOption);
+		}
+
+		var { valueLink, value, onChange, ...other } = this.props;
+		return (
+			<div className="row">
+			<div className="col pe-2">
+			&nbsp;
+			</div>
+			<div className="col">
+			<Select
+			name="site"
+			value={value || valueLink.value}
+			options={siteOptions}			   
+			multi="true"			    
+			onChange={this.onChange}
 			/>
-	      </div>
-	</div>
-    );
-  }
+			</div>
+			</div>
+		);
+	}
 });
 
 
@@ -99,41 +99,41 @@ var AlertSite = React.createClass({
 
 // http://voidcanvas.com/react-tutorial-two-way-data-binding/
 var AlertAnomalieType = React.createClass({	
-		getDefaultProps: function(){
-					return {
-						multiple: false,
-						name: 'Anomalie',
-					}
-				},					
-	                  //https://github.com/JedWatson/react-select/issues/256				
-					  onChange(textValue, arrayValue) {					  
-					     this.props.valueLink.requestChange(textValue);
-					  },
-  render: function() {
-	var options=[];
-    for (var i = 0; i < Object.keys(this.props.anomalie_types).length; i++) {
-		option={};
-	    option["value"] = i;
-	    option["label"] = Object.keys(this.props.anomalie_types)[i];
-	    options.push(option);
-	}
-	
-	var { valueLink, value, onChange, ...other } = this.props;
-    return (
-	     <div className="row" id="anomalierow">
-	      <div className="col pe-2">
-	        Anomalies;
-	      </div>
-	    <div className="col">
-		<Select
-			    name="anomalie"
-			    value={value || valueLink.value}
-			    options={options}			   
-			    multi="false"			    
-			    onChange={this.onChange}
+	getDefaultProps: function(){
+		return {
+			multiple: false,
+			name: 'Anomalie',
+		}
+	},					
+	//https://github.com/JedWatson/react-select/issues/256				
+	onChange(textValue, arrayValue) {					  
+		this.props.valueLink.requestChange(textValue);
+	},
+	render: function() {
+		var options=[];
+		for (var i = 0; i < Object.keys(this.props.anomalie_types).length; i++) {
+			option={};
+			option["value"] = i;
+			option["label"] = Object.keys(this.props.anomalie_types)[i];
+			options.push(option);
+		}
+
+		var { valueLink, value, onChange, ...other } = this.props;
+		return (
+			<div className="row" id="anomalierow">
+			<div className="col pe-2">
+			Anomalies;
+			</div>
+			<div className="col">
+			<Select
+			name="anomalie"
+			value={value || valueLink.value}
+			options={options}			   
+			multi="false"			    
+			onChange={this.onChange}
 			/>
-	      </div>
-	</div>
-    );
-  }
+			</div>
+			</div>
+		);
+	}
 });
