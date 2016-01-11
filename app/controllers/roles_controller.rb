0f0e1cc5ea2eb0433cb60520e7148a1ee0556f22
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2016-01-11 14:45:25 -0300

    For #670 display location and number of users in role list

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,8 @@ class RolesController < ApplicationController
   before_filter :load_institutions_and_sites, only: [:new, :create, :edit, :update]
 
   def index
-    @roles = check_access(Role, READ_ROLE).within(@navigation_context.entity)
+    @roles = check_access(Role, READ_ROLE).within(@navigation_context.entity).includes(:institution, :site)
+    @user_counts = check_access(Role, READ_ROLE).within(@navigation_context.entity).joins("LEFT JOIN roles_users ON roles.id = roles_users.role_id").group("roles.id").count(:user_id)
     @can_create = has_access?(Role, UPDATE_ROLE)
   end
 
