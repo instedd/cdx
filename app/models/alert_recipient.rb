10594e2e6061e821955abd5bbb1a0428045dbd54
Anthony O’Leary <anthony.oleary@finddx.org>
2016-01-27 16:29:56 +0000

    added very basic numericality validation for a telephone number - digits only for now

--- app/models/alert_recipient.rb
+++ app/models/alert_recipient.rb
@@ -10,16 +10,17 @@ class AlertRecipient < ActiveRecord::Base
   validate :email_telephone_validation_presence
 
   validates_format_of :email,:with => Devise.email_regexp
-
+  
+  validates :telephone, numericality: { only_integer: true }
   #TODO valid phone number: see  https://www.twilio.com/blog/2015/04/validate-phone-numbers-in-ruby-using-the-lookup-api.html
   
   private
 
   def name_validation
     if recipient_type == "external_user"
-      if first_name.length <= 2
+      if (first_name==nil) || (first_name.length <= 2)
         errors.add(:first_name, "first name too short")
-      elsif last_name.length <= 2
+      elsif (last_name==nil) || (last_name.length <= 2)
         errors.add(:last_name, "last name too short")
       end
     end
--- spec/models/alert_recipient_spec.rb
+++ spec/models/alert_recipient_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AlertRecipient, :type => :model do
   let!(:user) { User.make }
 
   context "validates fields" do
-    it "cannot create for invalid fields" do
+    it "cannot create for invalid name fields" do
       alert = Alert.create
       alertRecipient = AlertRecipient.new
       alertRecipient.recipient_type = AlertRecipient.recipient_types["external_user"]
@@ -22,13 +22,21 @@ RSpec.describe AlertRecipient, :type => :model do
       alertRecipient = AlertRecipient.new
       alertRecipient.recipient_type = AlertRecipient.recipient_types["external_user"]
       alertRecipient.email = "aa"
-      alertRecipient.telephone = "12456"
-      alertRecipient.first_name = ""
-      alertRecipient.last_name = "s"
       alertRecipient.alert=alert
 
       expect(alertRecipient).to_not be_valid
     end
+    
+      it "cannot create for invalid telephone field" do
+        alert = Alert.create
+        alertRecipient = AlertRecipient.new
+        alertRecipient.recipient_type = AlertRecipient.recipient_types["external_user"]
+        alertRecipient.telephone = "124a56"
+
+        alertRecipient.alert=alert
+
+        expect(alertRecipient).to_not be_valid
+      end
 
     it "cannot create for empty email and telephone fields" do
       alert = Alert.create
@@ -36,8 +44,6 @@ RSpec.describe AlertRecipient, :type => :model do
       alertRecipient.recipient_type = AlertRecipient.recipient_types["external_user"]
       alertRecipient.email = ""
       alertRecipient.telephone = ""
-      alertRecipient.first_name = ""
-      alertRecipient.last_name = "s"
       alertRecipient.alert=alert
 
       expect(alertRecipient).to_not be_valid
