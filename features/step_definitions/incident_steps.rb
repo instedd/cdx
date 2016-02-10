167414c642c2a191057f557a3179b214694d0be8
Anthony O’Leary <anthony.oleary@finddx.org>
2016-02-10 11:36:46 +0000

    added incident cucumber test

---
+++ features/step_definitions/incident_steps.rb
@@ -0,0 +1,6 @@
+Then(/^the user should see no incidents$/) do
+  @incidents = IncidentsPage.new
+  @incidents.load
+
+  expect(page).not_to have_xpath('//table/tr')  
+end
---
+++ features/support/page_objects/incident_page.rb
@@ -0,0 +1,5 @@
+class IncidentsPage < CdxPageBase
+  set_url "/incidents"
+
+  section :table, CdxTable, "table"
+end
