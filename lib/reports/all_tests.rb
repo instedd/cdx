ebd595f2cfce9bf9235668ce0bf37f019a9ffbbf
Stewart Campbell <stewartcuk@gmail.com>
2016-02-11 11:40:58 +0000

    Add service object for Error Reporting

--- lib/reports/all_tests.rb
+++ lib/reports/all_tests.rb
@@ -1,4 +1,18 @@
 module Reports
   class AllTests < Base
+    def self.by_name(*args)
+      new(*args).by_name
+    end
+
+    def by_name
+      filter['group_by'] = 'test.name'
+      results = TestResult.query(filter, current_user).execute
+      results['tests'].map do |test|
+        {
+          label: test['test.name'],
+          value: test['count']
+        }
+      end
+    end
   end
 end
--- lib/reports/base.rb
+++ lib/reports/base.rb
@@ -1,32 +1,18 @@
 module Reports
   class Base
-    def self.by_name(*args)
-      new(*args).by_name
-    end
-
     def self.process(*args)
       new(*args).process
     end
 
-    attr_reader :current_user, :context, :filter, :options, :results
+    attr_reader :current_user, :context, :data, :filter, :options, :results
 
     def initialize(current_user, context, options={})
       @filter ||= {}
       @current_user = current_user
       @context = context
+      @data = []
       @options = options
       setup
-    end
-
-    def by_name
-      filter['group_by'] = 'test.name'
-      results = TestResult.query(filter, current_user).execute
-      results['tests'].map do |test|
-        {
-          label: test['test.name'],
-          value: test['count']
-        }
-      end
     end
 
     def process
@@ -35,13 +21,12 @@ module Reports
     end
 
     def sort_by_month
-      data = []
       11.downto(0).each do |i|
         date = Date.today - i.months
         date_key = date.strftime('%Y-%m')
         date_results = results_by_day[date_key]
         data << {
-          label: "#{I18n.t("date.abbr_month_names")[date.month]}#{date.month == 1 ? " #{date.strftime("%y")}" : ""}",
+          label: label_monthly(date),
           values: [date_results ? date_results.count : 0]
         }
       end
@@ -49,6 +34,10 @@ module Reports
     end
 
     private
+
+    def label_monthly(date)
+      "#{I18n.t("date.abbr_month_names")[date.month]}#{date.month == 1 ? " #{date.strftime("%y")}" : ""}"
+    end
 
     def report_since
       filter['since'] = options['since'] || (Date.today - 1.year).iso8601
@@ -68,6 +57,10 @@ module Reports
     def site_or_institution
       filter['institution.uuid'] = context.institution.uuid if context.institution
       filter['site.path'] = context.site.uuid if context.site
+    end
+
+    def users
+      results['tests'].group_by { |t| t['test.start_time'] }
     end
   end
 end
