2b18269eae4efd2368c3736a7550e3306ac6fda0
Anthony O’Leary <anthony.oleary@finddx.org>
2016-02-16 12:07:18 +0000

    fixed bug in incidents, it was giving an error when an alert was disabled

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,6 +1,6 @@
 class IncidentsController < ApplicationController
   respond_to :html, :json
-  expose(:incidents) { current_user.alert_histories.where({for_aggregation_calculation: false}) }
+  expose(:incidents) { current_user.alert_histories.where({for_aggregation_calculation: false}).joins(:alert).where('alerts.enabled=true') }
 
   before_filter do
     head :forbidden unless has_access_to_test_results_index?
