dfc55ed922398b3be55631784c0773503df6681c
Mariano Abel Coca <marianoabelcoca@gmail.com>
2016-02-17 17:47:05 -0300

    Fixed daily demo data spec

--- spec/jobs/daily_demo_data_spec.rb
+++ spec/jobs/daily_demo_data_spec.rb
@@ -10,34 +10,34 @@ describe DailyDemoData do
     institution = Institution.create! user_id: user.id, name: 'demo-institution'
     site =  Site.create! institution: institution, name: 'demo-site'
 
-    device_model1 = DeviceModel.create! name: 'demo_fifo_model', published_at: 1.day.ago
-    manifest1 = Manifest.create! device_model: device_model1, definition: IO.read(File.join(Rails.root, 'db', 'seeds', 'manifests', 'fio_manifest.json' ))
-    device1 = Device.create! site: site, institution: site.institution , name: 'demo-device_fifo', serial_number: '123',  device_model:  manifest1.device_model, time_zone: "UTC"  
+    device_model1 = DeviceModel.create! name: 'demo_fio_model', published_at: 1.day.ago
+    manifest1 = Manifest.create! device_model: device_model1, definition: IO.read(File.join(Rails.root, 'db', 'seeds', 'manifests', 'deki_reader_manifest.json' ))
+    device1 = Device.create! site: site, institution: site.institution , name: 'demo-device_fio', serial_number: '123',  device_model:  manifest1.device_model, time_zone: "UTC"
 
     device_model2 = DeviceModel.create! name: 'demo_cepheid_model', published_at: 1.day.ago
-    manifest2 = Manifest.create! device_model: device_model2, definition: IO.read(File.join(Rails.root, 'db', 'seeds', 'manifests', 'cepheid_gene_xpert_manifest.json' ))
-    device2 = Device.create! site: site, institution: site.institution , name: 'demo-device_cepheid', serial_number: '456',  device_model:  manifest2.device_model, time_zone: "UTC"  
+    manifest2 = Manifest.create! device_model: device_model2, definition: IO.read(File.join(Rails.root, 'db', 'seeds', 'manifests', 'genexpert_manifest.json' ))
+    device2 = Device.create! site: site, institution: site.institution , name: 'demo-device_cepheid', serial_number: '456',  device_model:  manifest2.device_model, time_zone: "UTC"
   end
     
   let(:worker) { DailyDemoData.new }
   
   describe '#perform' do
-    it "generates demo data when enabled" do      
+    it "generates demo data when enabled" do
       ENV['USE_DEMO_DATA']='true'
 
       allow(worker).to receive("get_repeat_demo_per_device").and_return(4)
-      expect(TestResult.count).to eq(0)  
+      expect(TestResult.count).to eq(0)
       worker.perform
-      expect(TestResult.count).to eq(4)  
+      expect(TestResult.count).to eq(4)
     end
     
-    it "generates no demo data when disabled" do      
+    it "generates no demo data when disabled" do
       ENV['USE_DEMO_DATA']=nil
 
       allow(worker).to receive("get_repeat_demo_per_device").and_return(4)
-      expect(TestResult.count).to eq(0)  
+      expect(TestResult.count).to eq(0)
       worker.perform
-      expect(TestResult.count).to eq(0)  
+      expect(TestResult.count).to eq(0)
       end
       
   end
