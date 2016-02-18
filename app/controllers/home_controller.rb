b54d1b26b5e3ccd2ccca4820f48f2b823d5170eb
Brian J. Cardiff <bcardiff@manas.com.ar>
2016-02-18 16:52:32 -0300

    fix #775 add settings page move alerts, roles and policies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,20 @@ class HomeController < ApplicationController
   def index
   end
 
+  def settings
+    if has_access_to_roles_index?
+      @roles_count = check_access(Role, READ_ROLE).within(@navigation_context.entity, @navigation_context.exclude_subsites).count
+    end
+
+    if can_delegate_permissions?
+      @policies_count = current_user.granted_policies.count
+    end
+
+    if has_access_to_test_results_index?
+      @alerts_count = current_user.alerts.count
+    end
+  end
+
   def verify
     render layout: "messages"
   end
