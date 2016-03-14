66ff3ca65a8c4eb0551c089cb3d4cec37c57541b
Stewart Campbell <stewartcuk@gmail.com>
2016-03-14 16:55:42 +0000

    Correct device model error results

--- lib/reports/model_errors.rb
+++ lib/reports/model_errors.rb
@@ -14,28 +14,36 @@ module Reports
 
     private
 
-    def data_hash_day(dayname, day_results)
+    def data_hash_day(dayname, model_errors)
       {
         label: dayname,
         values: device_models.map do |u|
-          device_model_result = date_results && date_results[u]
-          device_model_result ? device_model_result['count'] : 0
+          model_error_result = model_errors && model_errors[u]
+          model_error_result ? count_total(model_error_result) : 0
         end
       }
     end
 
-    def data_hash_month(date, month_results)
+    def data_hash_month(date, model_errors)
       {
         label: label_monthly(date),
         values: device_models.map do |u|
-          device_model_result = month_results && month_results[u]
-          device_model_result ? device_model_result['count'] : 0
+          model_error_result = model_errors && model_errors[u]
+          model_error_result ? count_total(model_error_result) : 0
         end
       }
     end
 
-    def month_results(key)
-      results_by_period[key].try { |r| r.index_by { |t| t['device.model'] } }
+    def day_results(format='%Y-%m-%d', key)
+      results_by_period(format)[key].try do |r|
+        r.group_by { |t| t['device.model'] }
+      end
+    end
+
+    def month_results(format='%Y-%m', key)
+      results_by_period(format)[key].try do |r|
+        r.group_by { |t| t['device.model'] }
+      end
     end
   end
 end
