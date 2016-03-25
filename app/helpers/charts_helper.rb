3cfbfe5512052b0ce7b83908ab1b96a896fd83c1
Anthony O’Leary <anthony.oleary@finddx.org>
2016-03-25 10:58:58 +0000

    added device error table chart

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -13,6 +13,37 @@ module ChartsHelper
     end 
     return sites
   end
+ 
+  
+  def query_devices_not_reporting_chart
+    @devices = check_access(Device.within(@navigation_context.entity), Policy::Actions::READ_DEVICE)
+    if options["since"] !=nil
+      since=options["since"]
+    else
+      since = (Time.now - 12.months).strftime('%Y-%m-%d')
+    end
+        
+    data=[]
+    @devices.each do |device| 
+      if options["date_range"] ==nil
+        day_range =  (Date.parse(Time.now.strftime('%Y-%m-%d')) -  Date.parse(since) ).to_i
+        device_message = DeviceMessage.where(:device_id => device.id).where("created_at > ?", since).order(:created_at).first
+      else
+        day_range = ( Date.parse(options["date_range"]["start_time"]["lte"]) -  Date.parse(options["date_range"]["start_time"]["gte"]) ).to_i
+        device_message = DeviceMessage.where(:device_id => device.id).where("created_at > ? and created_at < ?",options["date_range"]["start_time"]["lge"],options["date_range"]["start_time"]["lte"]).order(:created_at).first
+      end
+      
+      if device_message==nil
+        data << [device.name, day_range]
+      else
+        days_diff = ( (Time.now - (device_message.created_at) ) / (1.day)).round
+        data << [device.name, days_diff]
+      end
+    end
+    
+    data
+  end
+  
   
   def devices_reporting_chart
     results = Reports::Devices.process(current_user, @navigation_context, options)
@@ -23,6 +54,18 @@ module ChartsHelper
   def errors_by_code
     Reports::Grouped.by_error_code(current_user, @navigation_context, options)
   end
+
+
+  def error_codes_by_device
+    data = Reports::DeviceErrorCodes.process(current_user, @navigation_context, options) 
+    data.get_device_location_details      
+  end
+
+  def outstanding_orders
+    data = Reports::OutstandingOrders.process(current_user, @navigation_context, options) 
+    data.latest_encounter
+  end
+  
 
   def errors_by_device_chart
     results = Reports::DeviceErrors.process(current_user, @navigation_context, options)
