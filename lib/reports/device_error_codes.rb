affe01759cc13a1064b413a5ee2a8deb5c0318d8
Anthony O’Leary <anthony.oleary@finddx.org>
2016-03-29 16:46:11 +0100

    added latest error date to error device chart table

--- lib/reports/device_error_codes.rb
+++ lib/reports/device_error_codes.rb
@@ -12,18 +12,29 @@ module Reports
       super
     end
     
-    def get_device_location_details
+    def get_device_location_details      
       data = results['tests'].map do |result|
       {
         device: Device.where(uuid: result["device.uuid"]).pluck(:name)[0],
         error_code: result["test.error_code"],
         count: result["count"],
-        location: Site.where(location_geoid: result["location.id"]).pluck(:name)
+        location: Site.where(location_geoid: result["location.id"]).pluck(:name),
+        last_error: latest_error_date(result)
       }
       end
     end
 
     private
+    
+    def latest_error_date(result)
+      filter = {}
+      filter["device.uuid"] =result["device.uuid"]
+      filter["error_code"] =result["test.error_code"]
+      filter["page_size"] ="1"
+      filter["order_by"]="-test.updated_time"
+      result = TestResult.query(filter, current_user).execute
+      result["tests"][0]["test"]["updated_time"].to_time.strftime("%Y-%m-%d %H:%M:%S")
+    end
 
     def day_results(format, key)
       super
