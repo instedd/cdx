319bd7fe49c2b4662c10c2f8883af88d1dfdcf11
Stewart Campbell <stewartcuk@gmail.com>
2016-04-06 13:40:24 +0100

    fixing site reports

--- lib/reports/site.rb
+++ lib/reports/site.rb
@@ -5,7 +5,30 @@ module Reports
       super
     end
 
+    def sites
+      site_uuids.inject({}) { |h, uuid| h[uuid] = lookup_site(uuid); h }
+    end
+
     def sort_by_site
+      site_results.each do |uuid, results|
+        data << [sites[uuid], count_total(results)]
+      end
+      return self
+    end
+
+    private
+
+    def lookup_site(uuid)
+      site = ::Site.where(uuid: uuid).first
+      return site.name if site
+    end
+
+    def site_results
+      results['tests'].group_by { |t| t['site.uuid'] }
+    end
+
+    def site_uuids
+      site_results.keys
     end
   end
 end
