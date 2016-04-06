d55891246089e6210dd39590b663ae0152e39f11
Anthony O’Leary <anthony.oleary@finddx.org>
2016-04-06 12:20:42 +0100

    added review comment changes

--- lib/reports/outstanding_orders.rb
+++ lib/reports/outstanding_orders.rb
@@ -1,17 +1,13 @@
 module Reports
   class OutstandingOrders  < Base
 
-    def process
-      super
-    end
-
     def latest_encounter
       @filter["group_by"]=nil
       encounters = Encounter.query(@filter, current_user).execute
       encounter_uuid_list=[]
       encounters["encounters"].each do |data|
         encounter = data["encounter"]
-        person = get_person_name(encounter)
+        person = full_name(encounter)
         encounter_uuid_list << {:test_order => encounter["uuid"],:date_ordered => Date.parse(encounter["start_time"]).strftime('%Y-%m-%d'),:test_result_encounter_end_time =>0,:outstanding => 0, :ordered_by => person}
       end
       #TODO , order results by time desc to remove a time check below: @filter['order_by']='-test.reported_time'
@@ -28,7 +24,7 @@ module Reports
         uuid = encounter["uuid"]
         matched_encounter= encounter_uuid_list.find {|x| x[:test_order] == uuid}
         if matched_encounter == nil
-          Rails.logger.info("no matched encounter for "+ uuid)
+          Rails.logger.info("no matched encounter for #{uuid}")
         else
           end_time = encounter["end_time"]
        #   end_time = result["test"]["reported_time"]         
@@ -46,19 +42,9 @@ module Reports
 
     private
 
-    def get_person_name(encounter)
-      person = encounter["user_email"]
-      if person==nil
-        person=""
-      else
-        user_person = User.where("email=?",person).pluck(:first_name, :last_name)
-        if (user_person.length>0)
-          person = user_person[0][0]+ " "+user_person[0][1]
-        else
-          person="--"
-        end
-      end
-      return person
+    def full_name(encounter)
+      return "" unless encounter['user_email']
+      return User.where('email = ?', encounter['user_email']).first.full_name
     end
 
   end
