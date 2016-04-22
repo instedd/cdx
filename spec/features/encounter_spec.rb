b45f5e2ef45f4c66fe8f19f103345492e34e77f3
Anthony O’Leary <anthony.oleary@finddx.org>
2016-04-22 09:55:48 +0100

    removed unused tests for encounter since the sample and test results are not displayed

--- spec/features/encounter_spec.rb
+++ spec/features/encounter_spec.rb
@@ -81,7 +81,7 @@ describe "create encounter" do
       end
     end
 
-    xit "should only show sites with create encounter permission of context institution" do
+    it "should only show sites with create encounter permission of context institution" do
       grant other_institution.user, user, site1, READ_SITE
       grant other_institution.user, user, site1, CREATE_SITE_ENCOUNTER
       grant other_institution.user, user, {encounter: site1}, READ_ENCOUNTER
@@ -102,131 +102,6 @@ describe "create encounter" do
     end
   end
 
-  xit "should search sample by id substring" do
-    process sample: {id: "ab111"}
-    process sample: {id: "22ab2"}
-    process sample: {id: "xy333"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_append_sample do |modal|
-        modal.perform_search "ab"
-        expect(modal.results.count).to eq(2)
-      end
-    end
-  end
-
-  xit "should search test by id substring" do
-    process test: {id: "ab111"}
-    process test: {id: "22ab2"}
-    process test: {id: "xy333"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_add_tests do |modal|
-        modal.perform_search "ab"
-        expect(modal.results.count).to eq(2)
-      end
-    end
-  end
-
-  xit "should add sample to encounter on save" do
-    process sample: {id: "ab111"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_append_sample.search_and_select_first "ab"
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      encounter = Encounter.find(page.id)
-      expect(encounter.samples).to match([Sample.first])
-    end
-  end
-
-  xit "should add test to encounter on save" do
-    process test: {id: "ab111"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_add_tests.search_and_select_first "ab"
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page.encounter.test_results).to match([TestResult.first])
-    end
-  end
-
-  xit "should create patient when adding test with information" do
-    process test: {id: "ab123"}, patient: {name: "John Doe"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_add_tests.search_and_select_first "ab123"
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page).to have_content "John Doe"
-      expect(page.encounter.patient.plain_sensitive_data["name"]).to match("John Doe")
-    end
-  end
-
-  xit "should merge patient information coming from different tests" do
-    process test: {id: "ab123"}, patient: {name: "John Doe"}
-    process test: {id: "ab456"}, patient: {gender: "male"}
-    process test: {id: "ab789"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_add_tests.search_and_select_first "ab123"
-      page.open_add_tests.search_and_select_first "ab456" # this is not added due to multiple patients
-      page.open_add_tests.search_and_select_first "ab789"
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page.encounter.test_results).to have(3).items
-      expect(page.encounter.patient.plain_sensitive_data["name"]).to match("John Doe")
-      expect(page.encounter.patient.core_fields["gender"]).to match("male")
-    end
-  end
-
-  xit "should be able to change the assay result and quantity of encounter" do
-    process test: {id: "a"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_add_tests.search_and_select_first "a"
-
-      page.diagnosis.assays.first.result.set "Negative"
-      page.diagnosis.assays.first.quant.set 5
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page).to have_content("Negative")
-      expect(page).to have_content("5")
-
-      page.submit
-    end
-
-    expect_page EditEncounterPage do |page|
-      expect(page.diagnosis.assays.first.result.value).to eq("Negative")
-      expect(page.diagnosis.assays.first.quant.value).to eq("5")
-
-      page.diagnosis.assays.first.result.set "Indeterminate"
-      page.diagnosis.assays.first.quant.set 3
-
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page).to have_content("Indeterminate")
-      expect(page).to have_content("3")
-    end
-  end
-
   context "adding test from other encounter" do
     it "should leave one encounter"
     it "should use encounter's diagnosis for merging"
@@ -237,27 +112,7 @@ describe "create encounter" do
     it "should leave one encounter"
   end
 
-  xit "adding test to existing sample should leave new test in encounter" do
-    process sample: {id: "s"}
-
-    goto_page NewEncounterPage do |page|
-      page.open_append_sample.search_and_select_first "s"
-      page.submit
-    end
-
-    expect_page ShowEncounterPage do |page|
-      expect(page.encounter.test_results.count).to eq(1)
-    end
-
-    process sample: {id: "s"}
-
-    visit current_path
-    expect_page ShowEncounterPage do |page|
-      expect(page.encounter.test_results.count).to eq(2)
-    end
-  end
-
-  xit "should be able to create fresh encounter with existing patient" do
+  it "should be able to create fresh encounter with existing patient" do
     patient = institution.patients.make name: Faker::Name.name, site: site
 
     goto_page NewFreshEncounterPage do |page|
@@ -275,7 +130,7 @@ describe "create encounter" do
     end
   end
 
-  xit "should be able to create fresh encounter with new patient" do
+  it "should be able to create fresh encounter with new patient" do
     goto_page NewFreshEncounterPage do |page|
       page.new_patient.click
     end
