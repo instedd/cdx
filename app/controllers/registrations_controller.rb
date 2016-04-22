729ebcf1b8bfe248f7626799651c4512623a9c2c
Mariano Abel Coca <marianoabelcoca@gmail.com>
2016-04-22 09:18:37 -0300

    Sidebar won't collapse when an error occurs updating user settings.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   before_filter :load_locales
-  skip_before_filter :ensure_context, except: :edit
+  skip_before_filter :ensure_context, except: [:edit, :update]
 
   protected
 
