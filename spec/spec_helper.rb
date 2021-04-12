6004b718bfc747761fa5e3f9f13f1fb3e2746fe6
Franciscello <ftarulla@gmail.com>
2021-04-12 16:17:14 -0300

    Remove test in features

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,6 +96,8 @@ RSpec.configure do |config|
       config.testrail_formatter_options[:disabled] = true
     end
   end
+
+  config.exclude_pattern = "spec/features/**/*_spec.rb"
 end
 
 require "bundler/setup"
