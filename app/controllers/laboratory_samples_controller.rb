e65eeeb0df7e234d54906ff3ee87f01c7f60e500
Franciscello <ftarulla@gmail.com>
2021-05-04 14:55:58 -0300

    Add LaboratorySample Create (#1088)
    * Add Create Laboratory Sample
    
    * Fix new sample uuid in form
    
    * Delete leftover from previous implementation
    
    This method was added back in e939086ffb1ed84bfa0d7fcea21b64480276fef2, but it isn't used now.
    
    Co-authored-by: Matías García Isaía <mgarcia@manas.tech>

--- app/controllers/laboratory_samples_controller.rb
+++ app/controllers/laboratory_samples_controller.rb
@@ -2,11 +2,36 @@ class LaboratorySamplesController < ApplicationController
 
   def index
     # TODO: filter by Institution
-    @samples = LaboratorySample.all
+    @samples = LaboratorySample.all.order('created_at DESC')
 
     @samples = @samples.where("uuid LIKE concat('%', ?, '%')", params[:uuid]) unless params[:uuid].blank?
 
     # paginate samples
     @samples = perform_pagination(@samples)
+  end
+
+  def new
+    @sample = LaboratorySample.new
+    session[:creating_sample_uuid] = @sample.uuid
+  end
+
+  def create
+    uuid = session[:creating_sample_uuid]
+
+    if uuid.blank?
+      redirect_to new_laboratory_sample_path, notice: 'There was an error creating the sample'
+      return
+    end
+
+    @sample = LaboratorySample.new({:uuid => uuid})
+
+    if @sample.save
+      session.delete(:creating_sample_uuid)
+
+      next_url = laboratory_samples_path(@sample)
+      redirect_to next_url, notice: 'Sample was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 end
