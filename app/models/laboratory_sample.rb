b82628f982a3b1d9a8c970df683f44ba51eed7f6
Franciscello <ftarulla@gmail.com>
2021-05-10 11:29:34 -0300

    Add Institution to LaboratorySample (#1095)

--- app/models/laboratory_sample.rb
+++ app/models/laboratory_sample.rb
@@ -1,6 +1,9 @@
 class LaboratorySample < ActiveRecord::Base
   include AutoUUID
 
+  belongs_to :institution
+  validates_presence_of :institution
+
   enum sample_type: { Specimen: 'specimen', QC: 'qc' }
 
   validates_presence_of :sample_type
