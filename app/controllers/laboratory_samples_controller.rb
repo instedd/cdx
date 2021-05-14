e70a71fbf1a531675d8c3e7ffde365fec80b6751
Franciscello <ftarulla@gmail.com>
2021-05-14 13:09:54 -0300

    Add print selected LaboratorySamples in list (#1097)
    * Add LaboratorySample bulk-print in List view
    
    * Fix layout: align table title and actions
    
    * Fix: download file, do not open new tab or navigate
    
    * Fix bulk-print filename suffix
    
    * Handle error when creating pdf file

--- app/controllers/laboratory_samples_controller.rb
+++ app/controllers/laboratory_samples_controller.rb
@@ -46,10 +46,41 @@ class LaboratorySamplesController < ApplicationController
     render pdf: "cdx_sample_#{@sample.uuid}",
       template: 'laboratory_samples/barcode.pdf',
       layout: 'layouts/pdf.html',
+      locals: { :sample => @sample },
       margin: { top: 2, bottom: 0, left: 0, right: 0 },
       page_width: '4in',
       page_height: '1.5in',
       show_as_html: params.key?('debug')
+  end
+
+  def bulk_print
+    @samples = LaboratorySample.where(id: params[:sample_ids])
+
+    if @samples.blank?
+      redirect_to laboratory_samples_path, notice: 'Select at least one sample to print.'
+      return
+    end
+
+    sample_strings = @samples.map do |sample|
+      render_to_string template: 'laboratory_samples/barcode.pdf',
+        layout: 'layouts/pdf.html',
+        locals: {:sample => sample}
+    end
+
+    options = {
+      margin: { top: 2, bottom: 0, left: 0, right: 0 },
+      page_width: '4in',
+      page_height: '1.5in'
+    }
+
+    begin
+      pdf_file = MultipagePdfRenderer.combine(sample_strings, options)
+      pdf_filename = "cdx_samples_#{@samples.size}_#{DateTime.now.strftime('%Y%m%d-%H%M')}.pdf"
+
+      send_data pdf_file, type: 'application/pdf', filename: pdf_filename
+    rescue
+      redirect_to laboratory_samples_path, notice: 'There was an error creating the print file.'
+    end
   end
 
   def edit
