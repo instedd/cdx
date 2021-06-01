c4e28e0b49cf5eab4faa73f3b6be0618d9014c21
devduarte <dduarte@manas.com.ar>
2021-06-01 17:56:36 -0300

    Add samples list and navigation to Batches edit

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -29,6 +29,7 @@ class BatchesController < ApplicationController
     batch = Batch.find(params[:id])
     @batch_form = BatchForm.edit(batch)
     @can_edit_sample_quantity = false
+    @samples = LaboratorySample.where(batch: batch)
   end
 
   def update
