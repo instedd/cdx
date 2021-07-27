92c431ade85a78e27e94bdc4c8efb8335a9ee28a
Franciscello <ftarulla@gmail.com>
2021-07-27 19:23:14 -0300

    Allow to upload any type of files for assays and download assays (#1281)

--- app/models/assay.rb
+++ app/models/assay.rb
@@ -1,6 +1,11 @@
 class Assay < ActiveRecord::Base
   belongs_to :sample
-  has_attached_file :picture, styles: { card: "130x130>" }, default_url: "card-unkown.png"
+  has_attached_file :picture, styles:  lambda { |a| a.instance.is_image? ? { :card => "130x130>" }  : {} }
 
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  validates_attachment_size :picture, :in => 0.megabytes..10.megabytes
+  do_not_validate_attachment_file_type :picture
+
+  def is_image?
+    self.picture.content_type =~ /\Aimage\/.*\Z/
+  end
 end
