14e3e486ab85a8ce76e05131c3f29e1f47098581
Franciscello <ftarulla@gmail.com>
2021-07-27 17:30:19 -0300

    Fix UI: adding new assays

--- app/models/assay.rb
+++ app/models/assay.rb
@@ -2,7 +2,7 @@ class Assay < ActiveRecord::Base
   belongs_to :sample
   has_attached_file :picture, styles:  lambda { |a| a.instance.is_image? ? { :card => "130x130>" }  : {} }
 
-  validates_attachment_size :picture, :in => 0.megabytes..2.megabytes
+  validates_attachment_size :picture, :in => 0.megabytes..10.megabytes
   do_not_validate_attachment_file_type :picture
 
   def is_image?
