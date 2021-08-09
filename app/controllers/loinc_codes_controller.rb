143d41cd8e8d211eeaf57efa86aca49c47e026e1
Franciscello <ftarulla@gmail.com>
2021-08-09 15:33:24 -0300

    (WIP)  Loinc code selector layout

--- app/controllers/loinc_codes_controller.rb
+++ app/controllers/loinc_codes_controller.rb
@@ -1,12 +1,12 @@
 class LoincCodesController < ApplicationController
   def search
     @loinc_codes = LoincCode.where("component LIKE concat('%', ?, '%')", params[:q])
-    @loinc_codes = @loinc_codes.page(1).per(10)
+    @loinc_codes = @loinc_codes.page(1).per(50)
 
     builder = Jbuilder.new do |json|
       json.array! @loinc_codes do |loinc_code|
         json.data loinc_code.loinc_number
-        json.value loinc_code.component
+        json.value "#{loinc_code.id} - #{loinc_code.component}"
         json.id loinc_code.id
       end
     end
