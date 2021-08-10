2c4248c3f4ee2cd5338fa24753b0efac37722301
Franciscello <ftarulla@gmail.com>
2021-08-10 11:59:39 -0300

    Move LoincCode import to script folder. Update README

--- script/loinc_codes/import_csv.rb
+++ script/loinc_codes/import_csv.rb
@@ -1,7 +1,10 @@
 require 'csv'
 
-csv_text = File.read('LoincTableCore.csv')
+csv_text = File.read('script/loinc_codes/LoincTableCore.csv')
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+puts "Start importing Loinc Codes ..."
+
 csv.each do |row|
   loinc_code = LoincCode.new
   loinc_code.loinc_number = row["LOINC_NUM"]
