c0f21ae63fd067ad6e731d5efea38bbb94259923
Franciscello <ftarulla@gmail.com>
2021-09-02 16:06:11 -0300

    Add QR to Sample print-label (#1316)

--- app/helpers/barcode_helper.rb
+++ app/helpers/barcode_helper.rb
@@ -1,6 +1,7 @@
 require 'barby'
 require 'barby/barcode/code_93'
 require 'barby/barcode/code_128'
+require 'barby/barcode/qr_code'
 require 'barby/outputter/html_outputter'
 require 'barby/outputter/png_outputter'
 
@@ -23,9 +24,12 @@ module BarcodeHelper
   end
 
   def barcode_img_data_url(code)
-    barcode = Barby::Code128.new(code)
+    barcode = Barby::QrCode.new(code, level: :m)
+
     outputter = Barby::PngOutputter.new(barcode)
     outputter.xdim = 2
+    # outputter.height = 50  # defaults: 100
+    outputter.margin = 0
     outputter.to_image.to_data_url
   end
 end
