4c6dab9e9644bca967f9c876942ff8b911032148
devduarte <dduarte@manas.com.ar>
2021-09-14 15:55:36 -0300

    change validation message for samples and batches. fix for #1326

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -26,8 +26,13 @@ class Batch < ActiveRecord::Base
   validates_presence_of :inactivation_method
   validates_inclusion_of :inactivation_method, in: INACTIVATION_METHOD_VALUES, message: "is not within valid options (should be one of #{INACTIVATION_METHOD_VALUES.join(', ')})"
 
-  SPECIMEN_ROLES = YAML.load_file(File.join(File.dirname(__FILE__), ".", "config", "specimen_roles.yml"))["specimen_roles"]
-  validates_inclusion_of :specimen_role, in: SPECIMEN_ROLES.map {|key, value| "#{key.upcase} - #{value}"}, allow_blank: true, message: "is not within valid options (should be one of #{SPECIMEN_ROLES.transform_keys(&:upcase).keys.join(', ')})"
+  SPECIMEN_ROLES_IDS = Batch.entity_fields.detect { |f| f.name == 'specimen_role' }.options
+  SPECIMEN_ROLES_DESCRIPTION = YAML.load_file(File.join(File.dirname(__FILE__), ".", "config", "specimen_roles.yml"))["specimen_roles"]
+  SPECIMEN_ROLES = SPECIMEN_ROLES_IDS.map { |id|
+    { id: id, description: "#{id.upcase} - #{SPECIMEN_ROLES_DESCRIPTION[id]}"}
+  }
+  validates_inclusion_of :specimen_role, in: SPECIMEN_ROLES_IDS, allow_blank: true, message: "is not within valid options (should be one of #{SPECIMEN_ROLES_IDS.map{|id| id.upcase}.join(', ')})"
+
 
   validates_presence_of :date_produced
   validates_presence_of :volume
