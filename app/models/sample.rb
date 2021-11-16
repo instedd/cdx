91edddd9202905f7e7fa0f1bfc380099a1fbe590
Franciscello <ftarulla@gmail.com>
2021-11-16 19:04:16 -0300

    Code cleanup. Fix note-sample relation

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -12,13 +12,13 @@ class Sample < ActiveRecord::Base
   has_many :sample_identifiers, inverse_of: :sample, dependent: :destroy
   has_many :test_results, through: :sample_identifiers
 
-  has_many :notes, dependent: :destroy
-  accepts_nested_attributes_for :notes, allow_destroy: true
-
   has_many :assay_attachments, dependent: :destroy
   accepts_nested_attributes_for :assay_attachments, allow_destroy: true
-
   validates_associated :assay_attachments, message: "are invalid"
+
+  has_many :notes, dependent: :destroy
+  accepts_nested_attributes_for :notes, allow_destroy: true
+  validates_associated :notes, message: "are invalid"
 
   validates_presence_of :institution
   validate :validate_encounter
