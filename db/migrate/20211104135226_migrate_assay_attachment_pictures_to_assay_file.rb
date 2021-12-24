d125aa7be87efedacd60291ec5f621880838a985
devduarte <dduarte@manas.com.ar>
2021-12-24 10:53:56 -0300

    fix typo in AssayAttachment to AssayFile migration

--- db/migrate/20211104135226_migrate_assay_attachment_pictures_to_assay_file.rb
+++ db/migrate/20211104135226_migrate_assay_attachment_pictures_to_assay_file.rb
@@ -7,9 +7,9 @@ class MigrateAssayAttachmentPicturesToAssayFile < ActiveRecord::Migration
       assay_file.picture_file_size = assay_attachment.picture_file_size
       assay_file.picture_updated_at = assay_attachment.picture_updated_at
       assay_file.assay_attachment = assay_attachment
-      if assay_file.save
-        assay_attachment.assay_File = assay_file
-        assay_attachment.save
+      if assay_file.save!
+        assay_attachment.assay_file = assay_file
+        assay_attachment.save!
       end
     end
 
