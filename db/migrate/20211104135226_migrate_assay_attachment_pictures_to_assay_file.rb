9617c97a58d45501e1360d700329e8b5be42e51a
devduarte <dduarte@manas.com.ar>
2021-12-28 10:37:46 -0300

    remove conditional on save method since save! raises an exception

--- db/migrate/20211104135226_migrate_assay_attachment_pictures_to_assay_file.rb
+++ db/migrate/20211104135226_migrate_assay_attachment_pictures_to_assay_file.rb
@@ -7,10 +7,9 @@ class MigrateAssayAttachmentPicturesToAssayFile < ActiveRecord::Migration
       assay_file.picture_file_size = assay_attachment.picture_file_size
       assay_file.picture_updated_at = assay_attachment.picture_updated_at
       assay_file.assay_attachment = assay_attachment
-      if assay_file.save!
-        assay_attachment.assay_file = assay_file
-        assay_attachment.save!
-      end
+      assay_file.save!
+      assay_attachment.assay_file = assay_file
+      assay_attachment.save!
     end
 
     remove_column :assay_attachments, :picture_file_name
