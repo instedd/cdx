7ded4a8dd279b28a329186627b28a2569e6a1113
devduarte <dduarte@manas.com.ar>
2022-01-18 08:19:21 -0300

    add validations to invite institution modal for preventing the user to send empty data that may cause an error

--- app/assets/javascripts/components/institution_invite_form.js.jsx
+++ app/assets/javascripts/components/institution_invite_form.js.jsx
@@ -1,15 +1,33 @@
 var InstitutionInviteForm = React.createClass({
   getInitialState: function() {
     return {
-      type: null,
-      name: ''
+      type: 'institution',
+      name: '',
+      hasTypeError: false,
+      hasNameError: false,
+      nextButtonDisabled: true
     };
   },
 
   changeType: function(newValue) {
-    this.setState({
-      type: newValue
-    });
+    if(this.isBlank(newValue)){
+      this.setState({
+        hasTypeError: true,
+        nextButtonDisabled: true
+      });
+    } else {
+      this.setState({
+        name: newValue,
+        hasTypeError: false
+      });
+
+      if(!this.isBlank(this.state.name)){
+        this.setState({
+          nextButtonDisabled: false
+        });
+      }
+
+    }
   },
 
   back: function() {
@@ -17,17 +35,38 @@ var InstitutionInviteForm = React.createClass({
   },
 
   next: function() {
-    this.props.adminInviteStep(this.state)
+    if(!this.state.hasTypeError && !this.state.hasNameError){
+      this.props.adminInviteStep(this.state)
+    }
   },
 
   cancel: function() {
     this.props.onFinished()
   },
 
+  isBlank: function(str) {
+    return (!str || /^\s*$/.test(str));
+  },
+
   setName: function(newName) {
-    this.setState({
-      name: newName
-    });
+    if(this.isBlank(newName)){
+      this.setState({
+        hasNameError: true,
+        nextButtonDisabled: true
+      });
+    } else {
+      this.setState({
+        name: newName,
+        hasNameError: false
+      });
+
+      if(!this.isBlank(this.state.type)){
+        this.setState({
+          nextButtonDisabled: false
+        });
+      }
+
+    }
   },
 
   componentDidMount: function() {
@@ -41,10 +80,22 @@ var InstitutionInviteForm = React.createClass({
           <div className="col pe-3"><label>Type</label></div>
           <div className="col"><CdxSelect name="type" items={this.props.types} value={this.state.type} onChange={this.changeType} /></div>
         </div>
+        { this.state.hasTypeError ?
+          <div className="col">
+            <span style={{ color: "red" }}>Required field</span>
+          </div>
+          : null
+        }
         <div className="row">
           <div className="col pe-3"><label>Name</label></div>
-          <div className="col"><input type="text" onChange={(e)=> {this.setName(e.currentTarget.value)}} /></div>
+          <div className="col"><input className="input-large" type="text" onChange={(e)=> {this.setName(e.currentTarget.value)}} /></div>
         </div>
+        { this.state.hasNameError ?
+          <div className="col">
+            <span style={{ color: "red" }}>Required field</span>
+          </div>
+          : null
+        }
         <div className="modal-footer">
           <div className="footer-buttons-aligning">
             <div>
@@ -52,7 +103,7 @@ var InstitutionInviteForm = React.createClass({
             </div>
             <div>
               <button className="btn btn-link" onClick={this.back}>Back</button>
-              <button className="btn btn-primary" onClick={this.next}>Next</button>
+              <button className="btn btn-primary" onClick={this.next} disabled={this.state.nextButtonDisabled}>Next</button>
             </div>
           </div>
         </div>
