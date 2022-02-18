dfe528e2d2b434c269ecdd144b3cc21affbd2d9f
Julien Portalier <julien@portalier.com>
2022-02-18 19:12:49 +0100

    Fix: don't silence errors in assay_files#create

--- app/controllers/assay_files_controller.rb
+++ app/controllers/assay_files_controller.rb
@@ -2,17 +2,19 @@ class AssayFilesController < ApplicationController
   def create
     assay_files = params.require(:assay_files)
     assay_files_data = {}
-    AssayFile.transaction do
-      begin
+
+    begin
+      AssayFile.transaction do
         assay_files.each do |index, file|
-          assay_file = AssayFile.new(picture: file)
-          assay_file.save!
+          assay_file = AssayFile.create!(picture: file)
           assay_files_data[assay_file.picture_file_name] = assay_file.id
         end
-        render json: {status: :ok, assay_files_data: assay_files_data}
-      rescue
-        render json: {status: :error}
       end
+    rescue => exception
+      Raven.capture_exception(exception)
+      render json: {status: :error}, status: :internal_server_error
+    else
+      render json: {status: :ok, assay_files_data: assay_files_data}
     end
   end
 end
