fac443933c5d0c10dc8d59baf2cc9487da7744c7
Julien Portalier <julien@portalier.com>
2022-03-15 17:27:03 +0100

    rails app:update + railsdiff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,24 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  if Rails::VERSION::MAJOR >= 5
+    # Enable/disable caching. By default caching is disabled.
+    if Rails.root.join('tmp/caching-dev.txt').exist?
+      config.action_controller.perform_caching = true
+
+      config.cache_store = :memory_store
+      config.public_file_server.headers = {
+        'Cache-Control' => 'public, max-age=172800'
+      }
+    else
+      config.action_controller.perform_caching = false
+
+      config.cache_store = :null_store
+    end
+  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -29,9 +44,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  if Rails::VERSION::MAJOR >= 5
+    # Suppress logger output for asset requests.
+    config.assets.quiet = true
+  end
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
@@ -40,6 +56,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  if Rails::VERSION::MAJOR > 5
+    # Use an evented file watcher to asynchronously detect changes in source code,
+    # routes, locales, etc. This feature depends on the listen gem.
+    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  end
 
   config.action_mailer.default_url_options = { host: Settings.host }
 
