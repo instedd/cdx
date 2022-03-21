be18ffa5fbda7857a0b22a253cad73bf1591076c
Diego Duarte <die.duarte@gmail.com>
2022-03-21 10:58:49 -0300

    Show no qc info warning and/or checkbox based selected samples (#1539)
    Co-authored-by: devduarte <dduarte@manas.com.ar>

--- app/assets/javascripts/components/sample_transfer.js.jsx
+++ app/assets/javascripts/components/sample_transfer.js.jsx
@@ -29,11 +29,23 @@ var SampleTransfer = React.createClass({
 
 var SampleTransferModal = React.createClass({
   getInitialState: function() {
+
     return {
       institutionId: null,
       includeQcInfo: false,
-      selectedSamples: selectedSamplesIds(),
+      selectedSamples: selectedSamplesIds()
     };
+  },
+
+  showQcWarningCheckbox: function(selectedSamples) {
+    const haveQc = selectedSamples.filter((sample) => sample.existsQcReference === true).length
+    const missingQuantity = selectedSamples.length - haveQc
+    return (
+      <div>
+        {haveQc > 0 && this.includeQcInfoCheckbox()}
+        {missingQuantity > 0 && this.qcInfoMessage(missingQuantity, selectedSamples)}
+      </div>
+    )
   },
 
   closeModal: function(event) {
@@ -96,6 +108,20 @@ var SampleTransferModal = React.createClass({
     )
   },
 
+  qcInfoMessage: function(missingQuantity, selectedSamples) {
+    const infoMessage = (missingQuantity > 0 && missingQuantity != selectedSamples.length)
+      ? `There is no Quality Control (QC) info available for ${missingQuantity} ${missingQuantity === 1 ? 'sample' : 'samples'}`
+      : "There is no Quality Control (QC) info available for these samples"
+
+    return (
+      <div className="row">
+        <div className="col icon-info-outline icon-gray qc-info-message">
+          <div className="notification-text">{infoMessage}</div>
+        </div>
+      </div>
+    )
+  },
+
   toggleQcInfo: function() {
     var oldValue = this.state.includeQcInfo;
     this.setState({
@@ -103,7 +129,16 @@ var SampleTransferModal = React.createClass({
     });
   },
 
-  render: function() {
+  includeQcInfoCheckbox: function () {
+    return (<div className="row">
+      <div className="col pe-3 qc-info-checkbox">
+        <input id="include-qc-check" type="checkbox" checked={this.state.includeQcInfo} onChange={this.toggleQcInfo}/>
+        <label htmlFor="include-qc-check">Include a copy of the QC data</label>
+      </div>
+    </div>)
+
+
+  }, render: function() {
     return(
       <div className="samples-transfer-modal">
         <div className="row">
@@ -116,12 +151,7 @@ var SampleTransferModal = React.createClass({
           <div className="col pe-3"><label>Institution</label></div>
           <div className="col"><CdxSelect name="institution" items={this.props.institutions} value={this.state.institutionId} onChange={this.changeInstitution} /></div>
         </div>
-        <div className="row">
-          <div className="col pe-3">
-            <input id="include-qc-check" type="checkbox" checked={this.state.includeQcInfo} onChange={this.toggleQcInfo} />
-            <label htmlFor="include-qc-check">Include a copy of the QC data</label>
-          </div>
-        </div>
+        {this.showQcWarningCheckbox(this.state.selectedSamples)}
         <div className="modal-footer">
           <div className="footer-buttons-aligning">
             <div>
