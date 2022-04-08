de2236bb67a5e7073e32cc262c3c49cbcbbaedbe
devduarte <dduarte@manas.com.ar>
2022-04-08 11:06:04 -0300

    fix user invites modal reference in index.haml

--- app/assets/javascripts/components/user_institution_invite.js.jsx
+++ app/assets/javascripts/components/user_institution_invite.js.jsx
@@ -7,24 +7,18 @@ var AddUserLink = React.createClass({
   },
 
   openInviteModal: function(event) {
-    this.refs.inviteModal.show();
+    this.refs.userInviteModal.show();
     event.preventDefault();
   },
 
   closeInviteModal: function() {
-    this.refs.inviteModal.hide();
-  },
-
-  componentDidMount: function () {
-    if(window.location.href.indexOf('#inviteInstitutions') != -1) {
-      this.openInviteModal()
-    }
+    this.refs.userInviteModal.hide();
   },
 
   render: function() {
     return (<div>
       <a className="btn-add icon side-link" href='#' title="Invite users" onClick={this.openInviteModal} ><span className="icon-mail icon-white"></span></a>
-      <Modal ref="inviteModal">
+      <Modal ref="userInviteModal">
         <h1>{this.state.title}</h1>
 
         <UserInviteForm onFinished={this.closeInviteModal} roles={this.props.roles} context={this.props.context}/>
