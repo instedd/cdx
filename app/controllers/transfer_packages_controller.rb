44557457f2145015c2f7ab79263af9f573cf030b
Johannes Müller <straightshoota@gmail.com>
2022-04-27 10:51:22 +0200

    Fix permit params

--- app/controllers/transfer_packages_controller.rb
+++ app/controllers/transfer_packages_controller.rb
@@ -22,17 +22,20 @@ class TransferPackagesController < ApplicationController
 
     samples = Sample
       .within(@navigation_context.institution)
-      .find_all_by_any_uuid(params[:sample_transfers_attributes].map { |_, sample_transfer| sample_transfer[:sample_uuid] })
+      .find_all_by_any_uuid(params[:sample_transfers_attributes].values.map { |sample_transfer| sample_transfer[:sample_uuid] })
 
-    params[:sample_transfers_attributes].each_value do |sample_transfer_attributes|
-      uuid = sample_transfer_attributes.delete(:sample_uuid)
-
+    params[:sample_transfers_attributes].transform_values! do |sample_transfer_attributes|
+      uuid = sample_transfer_attributes[:sample_uuid]
       sample = samples.find { |st| uuid == st.uuid }
       raise ActiveRecord::RecordNotFound unless sample
       raise "Can't transfer QC sample" if sample.is_quality_control?
       raise "User not authorized for transferring sample #{sample.uuid}" unless authorize_resource?(sample, UPDATE_SAMPLE)
-      sample_transfer_attributes[:sample] = sample
+
+      ActionController::Parameters.new(sample: sample).permit!
     end
+
+    # FIXME: permit! shouldn't be necessary here
+    params.permit!
 
     @transfer_package = TransferPackage.new(params)
 
