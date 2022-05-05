579396b13fa9f52bf3e0f7b04c6deb3604541daa
Johannes Müller <straightshoota@gmail.com>
2022-05-05 11:14:44 +0200

    Fix migration referencing delegated `SampleTransfer#sender_institution` (#1637)

--- db/migrate/20220412181435_change_sample_transfers_requires_transfer_package.rb
+++ db/migrate/20220412181435_change_sample_transfers_requires_transfer_package.rb
@@ -3,8 +3,8 @@ class ChangeSampleTransfersRequiresTransferPackage < ActiveRecord::Migration
     # Create packages for existing transfers
     SampleTransfer.where(transfer_package_id: nil).find_each do |transfer|
       package = TransferPackage.create(
-        sender_institution: transfer.sender_institution,
-        receiver_institution: transfer.receiver_institution,
+        sender_institution_id: transfer.sender_institution_id,
+        receiver_institution_id: transfer.receiver_institution_id,
       )
       transfer.update_columns(
         transfer_package_id: package.id,
