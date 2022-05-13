7534a1cd469841f9a0e4c167445d0b94842dfc04
Johannes Müller <straightshoota@gmail.com>
2022-05-13 17:09:45 +0200

    Add validation errors to `TransferPackage#create` form

--- spec/controllers/transfer_packages_controller_spec.rb
+++ spec/controllers/transfer_packages_controller_spec.rb
@@ -260,6 +260,7 @@ RSpec.describe TransferPackagesController, type: :controller do
       end.not_to change { TransferPackage.count }
 
       expect(response).to have_http_status(:ok)
+      expect(response.body).to include("Receiver institution can't be blank")
     end
 
     it "rejects transferring QC sample" do
