0c10cae2ff07611967a1939e962ede3f74e90bb8
Julien Portalier <julien@portalier.com>
2022-05-16 14:03:22 +0200

    Speedup multipage PDF labels rendering
    Renders a single HTML page to render as PDF using WickedPDF which
    results in a single invocation of wkhtmltopdf, instead of rendering
    each page individually and invoking wkhtmltopdf for each page.
    
    Reduces a render of 109 labels from 30s down to 9s.

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,5 +1,3 @@
-require "labels_pdf_renderer"
-
 class BoxesController < ApplicationController
   before_action :load_box, except: %i[index new create bulk_destroy]
 
@@ -25,32 +23,17 @@ class BoxesController < ApplicationController
   def print
     return unless authorize_resource(@box, READ_BOX)
 
-    pages = []
-
-    # render box label
-    pages << render_to_string(
-      template: "boxes/barcode.pdf",
-      "layout": "layouts/pdf.html",
-      locals: { box: @box }
-    )
-
-    # render samples' labels
-    @box.samples.preload(:sample_identifiers).each do |sample|
-      pages << render_to_string(
-        template: "samples/barcode.pdf",
-        "layout": "layouts/pdf.html",
-        locals: { sample: sample }
-      )
-    end
-
-    begin
-      send_data LabelsPdfRenderer.combine(pages),
-        type: "application/pdf",
-        filename: "cdx_box_#{DateTime.now.strftime("%Y%m%d-%H%M")}.pdf"
-    rescue => ex
-      Raven.capture_exception(ex)
-      redirect_to boxes_path, notice: "There was an error creating the print file."
-    end
+    render pdf: "cdx_box_#{@box.uuid}",
+      template: "boxes/print.pdf",
+      layout: "layouts/pdf.html",
+      locals: {
+        box: @box,
+        samples: @box.samples.preload(:sample_identifiers),
+      },
+      margin: { top: 0, bottom: 0, left: 0, right: 0 },
+      page_width: "1in",
+      page_height: "1in",
+      show_as_html: params.key?("debug")
   end
 
   def new
