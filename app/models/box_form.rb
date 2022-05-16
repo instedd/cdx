586eed0501c6fe23dc56381c74d2a96638072bad
Julien Portalier <julien@portalier.com>
2022-05-16 13:34:54 +0200

    Drop BoxForm#batch_errors

--- app/models/box_form.rb
+++ app/models/box_form.rb
@@ -1,5 +1,5 @@
 class BoxForm
-  attr_reader :box, :batch_numbers, :batch_errors
+  attr_reader :box, :batch_numbers
 
   delegate :purpose, :purpose=, to: :box
   delegate :model_name, :errors, to: :box
@@ -23,20 +23,14 @@ class BoxForm
   def initialize(box, batch_numbers)
     @box = box
     @batch_numbers = batch_numbers
-    @batch_errors = {}
   end
 
   def batches=(relation)
-    @batches = {}
     records = relation.to_a
 
-    @batch_numbers.each do |key, batch_number|
-      if batch = records.find { |b| b.batch_number == batch_number }
-        @batches[key] = batch
-      elsif batch_number.present?
-        @batch_errors[key] = "batch doesn't exist"
-      end
-    end
+    @batches = Hash[@batch_numbers.map do |key, batch_number|
+      [key, records.find { |b| b.batch_number == batch_number }]
+    end]
   end
 
   def build_samples
@@ -50,19 +44,21 @@ class BoxForm
       end
 
     when "Challenge"
-      @box.build_samples(@batches["virus"], exponents: [1, 4, 8], replicas: 18)
-
       @batches.each do |key, batch|
-        @box.build_samples(batch, exponents: [1, 4, 8], replicas: 3) unless key == "virus"
+        if key == "virus"
+          @box.build_samples(batch, exponents: [1, 4, 8], replicas: 18)
+        else
+          @box.build_samples(batch, exponents: [1, 4, 8], replicas: 3)
+        end
       end
     end
   end
 
   def valid?
     @box.valid?
-    @batch_errors.each { |key, message| @box.errors.add(key.to_sym, message) unless key.blank? }
-    validate_batches
-    @box.errors.empty? && @batch_errors.empty?
+    validate_existence_of_batches
+    validate_batches_for_purpose
+    @box.errors.empty?
   end
 
   def save
@@ -75,16 +71,22 @@ class BoxForm
 
   private
 
-  def validate_batches
-    count = @batches.map { |_, b| b.batch_number }.uniq.size
+  def validate_existence_of_batches
+    @batch_numbers.each do |key, batch_number|
+      unless batch_number.blank? || @batches[key]
+        @box.errors.add(key, "batch doesn't exist")
+      end
+    end
+  end
+
+  def validate_batches_for_purpose
+    count = @batches.map { |_, b| b.try(&:batch_number) }.uniq.size
 
     case @box.purpose
     when "LOD"
       @box.errors.add(:lod, "a batch is required") unless @batches["lod"]
-
     when "Variants"
       @box.errors.add(:base, "You must select at least two batches") unless count >= 2
-
     when "Challenge"
       @box.errors.add(:virus, "a virus batch is required") unless @batches["virus"]
       @box.errors.add(:base, "You must select at least one distractor batch") unless count >= 2
