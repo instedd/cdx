2b98438fdd95ed639dbf1155a687788cd5b59748
Johannes Müller <straightshoota@gmail.com>
2022-05-16 20:28:14 +0200

    Disable field_error_proc

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module Cdp
     config.action_mailer.asset_host = "http://#{Settings.host}"
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
+    # disable <div class="field_with_errors"> wrapper
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 
   def self.version
