6b79696e0caa86cec8fd97be5cb4f59a3fb5b7fb
Johannes Müller <straightshoota@gmail.com>
2022-06-15 16:59:36 +0200

    Drop migrations using `SampleTransfer`
    These data migrations are unnecessary and conflict with the removal of
    the `SampleTransfer` model.

--- db/migrate/20220412181435_change_sample_transfers_requires_transfer_package.rb
+++ db/migrate/20220412181435_change_sample_transfers_requires_transfer_package.rb
@@ -1,16 +1,5 @@
 class ChangeSampleTransfersRequiresTransferPackage < ActiveRecord::Migration
   def up
-    # Create packages for existing transfers
-    SampleTransfer.where(transfer_package_id: nil).find_each do |transfer|
-      package = TransferPackage.create(
-        sender_institution_id: transfer.sender_institution_id,
-        receiver_institution_id: transfer.receiver_institution_id,
-      )
-      transfer.update_columns(
-        transfer_package_id: package.id,
-      )
-    end
-
     change_column_null :sample_transfers, :transfer_package_id, false
   end
 
