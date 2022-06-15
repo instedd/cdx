1cfa383ffa93756c3884b75dbb17b70138ed772b
Johannes Müller <straightshoota@gmail.com>
2022-06-15 16:59:36 +0200

    Drop migrations using `SampleTransfer`
    These data migrations are unnecessary and conflict with the removal of
    the `SampleTransfer` model.

--- db/migrate/20220505070823_add_confirmed_at_to_transfer_packages.rb
+++ db/migrate/20220505070823_add_confirmed_at_to_transfer_packages.rb
@@ -4,15 +4,6 @@ class AddConfirmedAtToTransferPackages < ActiveRecord::Migration
     add_index :transfer_packages, :confirmed_at
 
     TransferPackage.reset_column_information
-
-    # Fill confirmed_at on existing packages if their sample transfers have been confirmed
-    TransferPackage.find_each do |package|
-      if confirmed_at = package.sample_transfers.map(&:confirmed_at).compact.max
-        package.update_columns(
-          confirmed_at: confirmed_at,
-        )
-      end
-    end
   end
 
   def down
