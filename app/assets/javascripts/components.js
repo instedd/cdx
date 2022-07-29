103a7d0fa33991e6ea3004930f3ad990cd22d5aa
Carlos Zillner <czillner@manas.com.ar>
2022-07-29 13:54:07 -0300

    Fix CSS Input Date For All Browsers
    I was changing the color for Chrome only. So I removed
    the webkit selector. I put a comment inside input date
    javascript too, to describe the problem and prevent
    other developers to waste time with this.

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -17,6 +17,19 @@ function cdx_select_on_change(name, callback) {
     callback($(this).val());
   });
 }
+
+/*
+This is kind of a exception solution. Because input date's behavior are different.
+Consider this points:
+  - input date's placeholders doesn't work
+  - even if at some point in the future it works, you cannot know what's the OS date format
+  - CSS selectors like :empty, :valid, [value=''], :placeholder-shown doesn't work
+  - date format's depends of the browser and OS
+  - Google Chrome has specific selector to color the content of this kind of fields, but other browser doesn't
+
+  Finally you can test if something changes on the future on this fiddle:
+    https://jsfiddle.net/omelao/hsk8vyax/15/
+*/
 $(document).on("change", "input[type=date]", function(){
   $(this).val() != '' ? $(this).addClass("filled") : $(this).removeClass("filled");
 });
