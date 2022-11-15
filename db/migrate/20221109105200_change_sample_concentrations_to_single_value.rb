f92fb75fc5e28c16ac407e507e4a6a5846a98f08
Julien Portalier <julien@portalier.com>
2022-11-15 15:26:41 +0100

    WIP: verify sample concentration db migration

--- db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
+++ db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
@@ -1,12 +1,11 @@
 class ChangeSampleConcentrationsToSingleValue < ActiveRecord::Migration
-
   def up
-    Sample.find_each do |sample|
-        number = sample.core_fields["concentration_number"].try(&:to_i)
-        exponent = sample.core_fields["concentration_exponent"].try(&:to_i)
-        if number && exponent
-          sample.core_fields["concentration"] = number * (10**exponent)
-          sample.save
+    Sample.preload(:assay_attachments, :notes, :institution, :box => :institution).find_each do |sample|
+      number = sample.core_fields["concentration_number"].try(&:to_i)
+      exponent = sample.core_fields["concentration_exponent"].try(&:to_i)
+      if number && exponent
+        sample.core_fields["concentration"] = number * (10**exponent)
+        sample.save!
       end
     end
   end
