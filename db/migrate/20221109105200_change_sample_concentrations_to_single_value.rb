77f82a62617e95d103dcb598982bb895882ddc88
Julien Portalier <julien@portalier.com>
2022-11-15 18:37:30 +0100

    Remove old concentration core fields for samples

--- db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
+++ db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
@@ -3,8 +3,11 @@ class ChangeSampleConcentrationsToSingleValue < ActiveRecord::Migration
     Sample.preload(:assay_attachments, :notes, :institution, :box => :institution).find_each do |sample|
       number = sample.core_fields["concentration_number"].try(&:to_i)
       exponent = sample.core_fields["concentration_exponent"].try(&:to_i)
+
       if number && exponent
         sample.core_fields["concentration"] = number * (10**exponent)
+        sample.core_fields.delete("concentration_number")
+        sample.core_fields.delete("concentration_exponent")
         sample.save!
       end
     end
