aba3420886006c368b951ce9ce9ab080e470d8d9
Julien Portalier <julien@portalier.com>
2022-11-15 14:24:39 +0100

    Fix: invalid migration

--- db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
+++ db/migrate/20221109105200_change_sample_concentrations_to_single_value.rb
@@ -2,8 +2,8 @@ class ChangeSampleConcentrationsToSingleValue < ActiveRecord::Migration
 
   def up
     Sample.find_each do |sample|
-        number = sample.core_fields.key?("concentration_number").try(&:to_i)
-        exponent = sample.core_fields.key?("concentration_exponent").try(&:to_i)
+        number = sample.core_fields["concentration_number"].try(&:to_i)
+        exponent = sample.core_fields["concentration_exponent"].try(&:to_i)
         if number && exponent
           sample.core_fields["concentration"] = number * (10**exponent)
           sample.save
