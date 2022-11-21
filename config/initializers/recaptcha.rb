618e59594b86862b2b8dcd2292f6d0ad4b7cb88c
Carlos Zillner <carlos@function.ws>
2022-11-21 11:15:21 -0300

    Cut Recaptcha Credentials and Add Enable Settings
    Co-authored-by: Julien Portalier <julien@portalier.com>

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key = Settings.recaptcha.site_key
+  config.secret_key = Settings.recaptcha.secret_key
 end
