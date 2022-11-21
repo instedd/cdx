7da9092d12c372c4755238d115be87534f216d1b
Carlos Zillner <carlos@function.ws>
2022-11-21 13:38:19 -0300

    Defend against spam (#1796)

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+if Settings.recaptcha.enabled
+  Recaptcha.configure do |config|
+    config.site_key = Settings.recaptcha.site_key
+    config.secret_key = Settings.recaptcha.secret_key
+  end
+end
