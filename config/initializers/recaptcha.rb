3d7e1fcb15777c06bdd7daa8ed337c478f5f7c12
Julien Portalier <julien@portalier.com>
2022-11-21 17:26:57 +0100

    Make recaptcha opt-in #3

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
-Recaptcha.configure do |config|
-  config.site_key = Settings.recaptcha.site_key
-  config.secret_key = Settings.recaptcha.secret_key
+if Settings.recaptcha.enabled
+  Recaptcha.configure do |config|
+    config.site_key = Settings.recaptcha.site_key
+    config.secret_key = Settings.recaptcha.secret_key
+  end
 end
