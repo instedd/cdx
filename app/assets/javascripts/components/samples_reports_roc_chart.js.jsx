8b710eefa46813460082b486f936aa072996b40d
leandroradusky <leandro.radusky@gmail.com>
2023-02-02 12:39:05 +0100

    Rename box reports as reports (#1889)
    * Rename box reports as reports
    
    * Adding Samples Reports as header

--- app/assets/javascripts/components/samples_reports_roc_chart.js.jsx
+++ app/assets/javascripts/components/samples_reports_roc_chart.js.jsx
@@ -1,4 +1,4 @@
-var BoxReportsRocChart = React.createClass({
+var SamplesReportsRocChart = React.createClass({
     getDefaultProps: function() {
       return {
         margin: {top: 50, right: 50, bottom: 50, left: 50},
