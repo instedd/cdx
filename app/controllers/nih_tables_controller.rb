70502dfe203f02924e83efe1645e08e2275221da
Leandro Radusky <leandro.radusky@gmail.com>
2023-07-03 17:05:12 +0200

    Fixes PR review

--- app/controllers/nih_tables_controller.rb
+++ app/controllers/nih_tables_controller.rb
@@ -1,45 +1,44 @@
 class NihTablesController < ApplicationController
   def show
-    samples_report = SamplesReport.find(params[:id])
-    return unless authorize_resource(samples_report, READ_SAMPLES_REPORT)
+    @samples_report = SamplesReport.find(params[:id])
+    return unless authorize_resource(@samples_report, READ_SAMPLES_REPORT)
     
-    zip_file = create_zip_file(samples_report)
+    zip_file = create_zip_file
     zip_file.close
   
-    send_zip_file(zip_file, samples_report.name)
+    send_zip_file(zip_file)
   end
   
   private
   
-  def create_zip_file(samples_report)
-    purpose = samples_report.samples[0].box.purpose
-    zip_file = Tempfile.new("#{samples_report.name}_nih_tables.zip")
+  def create_zip_file
+    purpose = @samples_report.samples[0].box.purpose
+    zip_file = Tempfile.new("#{@samples_report.name}_nih_tables.zip")
     Zip::File.open(zip_file.path, Zip::File::CREATE) do |zip|
       zip.add("Instructions.txt", Rails.root.join('public/templates/Instructions.txt'))
-      add_nih_table(samples_report, 'samples', zip)
-      add_nih_table(samples_report, 'results', zip)
+      add_nih_table('samples', zip)
+      add_nih_table('results', zip)
 
       if purpose == "LOD"
-        #add_nih_table(samples_report, 'lod', zip)
+        #add_nih_table('lod', zip)
       elsif purpose == "Challenge"
-        #add_nih_table(samples_report, 'challenge', zip)
+        #add_nih_table('challenge', zip)
       end
     end
     zip_file
   end
 
-  def add_nih_table(samples_report, table_name, zip_file)
-    csv_file = Tempfile.new("#{samples_report.name}_#{table_name}.csv")
+  def add_nih_table(table_name, zip_file)
+    csv_file = Tempfile.new("#{@samples_report.name}_#{table_name}.csv")
     
     csv_file.write(
-      render_to_string(:file => 'samples_reports/nih_'+table_name+'.csv.csvbuilder', :locals => { :samples_report => samples_report })
+      render_to_string('samples_reports/nih_'+table_name, formats: :csv)
     )
     csv_file.close
-
-    zip_file.add("#{samples_report.name}_#{table_name}.csv", csv_file.path)
+    zip_file.add("#{@samples_report.name}_#{table_name}.csv", csv_file.path)
   end
   
-  def send_zip_file(zip_file, filename)
-    send_file zip_file.path, type: 'application/zip', filename: "#{filename}_nih_tables.zip"
+  def send_zip_file(zip_file)
+    send_file zip_file.path, type: 'application/zip', filename: "#{@samples_report.name}_nih_tables.zip"
   end
 end
