ca5709dabc16985a6a9414978b1a734e0758beff
leandroradusky <leandro.radusky@gmail.com>
2023-11-03 10:33:17 +0100

    Migrations needed for modifications on the new batches NIH table (#1997)
    * Migrations needed for modifications on the new batches NIH table
    
    * Lint fixes
    
    * PR review suggestions

--- db/migrate/20231030102203_add_original_batch_id_to_samples.rb
+++ db/migrate/20231030102203_add_original_batch_id_to_samples.rb
@@ -1,5 +1,27 @@
 class AddOriginalBatchIdToSamples < ActiveRecord::Migration
   def change
     add_column :samples, :original_batch_id, :integer
+
+    Sample.preload(:batch).find_each do |sample|
+      if sample.batch
+        sample.original_batch_id = sample.batch.id
+        sample.save
+      elsif sample.box_id && sample.old_batch_number
+        institution_ids = TransferPackage
+          .select(:sender_institution_id)
+          .joins(:box_transfers)
+          .where(box_transfers: { box_id: sample.box_id })
+          .distinct
+      
+        original_batch = Batch.where(
+          institution_id: institution_ids,
+          batch_number: sample.old_batch_number
+        ).take
+        if original_batch
+          sample.original_batch_id = original_batch.id
+          sample.save
+        end
+      end
+    end
   end
 end
